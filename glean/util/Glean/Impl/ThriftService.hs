03de625c1fdba7356685e3363f2d2d79d7ed8c43
Simon Marlow <smarlow@fb.com>
2021-03-23 01:24:05 -0700

    Resolve host names in Glean.Impl.ThriftService
    Summary: This might not be the best place to do it, but it works.
    
    Reviewed By: nhawkes
    
    Differential Revision: D27188278
    
    fbshipit-source-id: 52ecc03909fe3d49e3f956ef9609a470544d1709

--- glean/util/Glean/Impl/ThriftService.hs
+++ glean/util/Glean/Impl/ThriftService.hs
@@ -3,10 +3,10 @@ module Glean.Impl.ThriftService
   ( ThriftService
   ) where
 
-import qualified Data.ByteString.Char8 as B
+import qualified Data.ByteString.UTF8 as UTF8
 import Data.Maybe
-import qualified Data.Text as Text
 import qualified Data.Text.Encoding as Text
+import Network.Socket
 
 import Thrift.Channel.HeaderChannel
 import Thrift.Protocol.Id
@@ -28,7 +28,7 @@ instance IsThriftService ThriftService where
     where
     timeout = round (fromMaybe 30 processingTimeout * 1000)
     headerConfig = HeaderConfig
-      { headerHost = B.pack (Text.unpack h)
+      { headerHost = Text.encodeUtf8 h
       , headerPort = fromIntegral p
       , headerProtocolId = compactProtocolId
       , headerConnTimeout = timeout
@@ -39,8 +39,21 @@ instance IsThriftService ThriftService where
 
   thriftServiceWithShard t _ = t  -- shards are irrelevant if we have host/port
 
-  runThrift evb ThriftService{..} action =
-    withHeaderChannel evb headerConfig action
+  runThrift evb ThriftService{..} action = do
+    addrs <- getAddrInfo
+      (Just defaultHints)
+      (Just (UTF8.toString (headerHost headerConfig)))
+      Nothing
+    headerConfig' <- case addrs of
+       [] -> return headerConfig
+       (addr : _) -> do
+         (mHost, _) <- getNameInfo [NI_NUMERICHOST] True False
+           (addrAddress addr)
+         case mHost of
+           Nothing -> return headerConfig
+           Just host -> return
+             headerConfig { headerHost = UTF8.fromString host }
+    withHeaderChannel evb headerConfig' action
 
   getSelection _evb ThriftService{..} _ =
     return
