02dbf36e98fd364cd888a9e0f29ddd7d27d2c67a
Simon Marlow <smarlow@fb.com>
2021-04-20 04:00:12 -0700

    Move thriftmangle test into glean/facebook/test
    Summary:
    Just tidying up, moving the indexer-specific tests next to the
    relevant indexers.
    
    Reviewed By: zsol
    
    Differential Revision: D27852376
    
    fbshipit-source-id: 766b3b7f2fbbe1cebd5848abaa97d8579a18a3b7

--- glean/test/regression/Glean/Regression/Driver/ThriftMangle.hs
+++
@@ -1,7 +0,0 @@
-module Glean.Regression.Driver.ThriftMangle (main) where
-
-import qualified Derive.ThriftMangle.RegressionTest (driver)
-import Glean.Regression.Test (testMain)
-
-main :: IO ()
-main = testMain Derive.ThriftMangle.RegressionTest.driver
