a93e83df7c65a67513311870c0c247f0d5ee3608
Simon Marlow <smarlow@fb.com>
2021-04-20 04:00:12 -0700

    Move thrift indexer tests to glean/lang/thrift/tests
    Summary:
    Just moving the indexer-specific test code out of
    `glean/test/regression` and next to the relevant indexers.
    
    Reviewed By: zsol
    
    Differential Revision: D27852275
    
    fbshipit-source-id: fe2d6d6278c0ccdd20c8124a0c20e3a17a40ba09

--- glean/test/regression/Glean/Regression/Driver/ThriftIndexer.hs
+++
@@ -1,7 +0,0 @@
-module Glean.Regression.Driver.ThriftIndexer (main) where
-
-import qualified Glean.ThriftIndexer.Test as ThriftIndexer
-import Glean.Regression.Test
-
-main :: IO ()
-main = testMain ThriftIndexer.driver
