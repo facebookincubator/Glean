aeb55fe772b0f96c99da1d765b9a36ddbfa22252
Simon Marlow <smarlow@fb.com>
2021-04-23 05:11:17 -0700

    Move Glean.Util.Benchmark under glean/util
    Summary:
    I've no idea why it ended up in glean/facebook, but it's needed for
    compiling the benchmarks.
    
    Reviewed By: nhawkes
    
    Differential Revision: D27912901
    
    fbshipit-source-id: 6b78b55efefdf558020a9b8273c629b6215ab712

---
+++ glean/util/Glean/Util/Benchmark.hs
@@ -0,0 +1,15 @@
+module Glean.Util.Benchmark
+  ( BenchmarkRunner
+  , benchmarkMain
+  ) where
+
+import Criterion.Main
+import Criterion.Main.Options
+
+import Glean.Init
+
+type BenchmarkRunner = [Benchmark] -> IO ()
+
+benchmarkMain :: (BenchmarkRunner -> IO ()) -> IO ()
+benchmarkMain exec = withOptions (describe defaultConfig) $
+  exec . runMode
