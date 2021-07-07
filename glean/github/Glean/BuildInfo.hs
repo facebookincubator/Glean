dfba2dde7a5e2da18c200857c4b6378b8dc44c3b
Simon Marlow <marlowsd@gmail.com>
2021-07-07 18:04:14 +0100

    Determine build time and revision

--- glean/github/Glean/BuildInfo.hs
+++ glean/github/Glean/BuildInfo.hs
@@ -1,8 +1,14 @@
 -- Copyright (c) Facebook, Inc. and its affiliates.
 
+{-# LANGUAGE TemplateHaskell #-}
 module Glean.BuildInfo (module Glean.BuildInfo) where
 
+import Control.Monad.IO.Class
 import Data.Text (Text)
+import Data.Time
+import Language.Haskell.TH
+import System.Exit
+import System.Process
 
 buildRule :: Text
 buildRule = "glean"
@@ -11,7 +17,16 @@ buildMode :: Text
 buildMode = "cabal"
 
 buildTimeISO8601 :: Text
-buildTimeISO8601 = "<time>"
+buildTimeISO8601 = $(do
+  now <- liftIO getCurrentTime
+  litE (stringL (show now))
+  )
 
 buildRevision :: Text
-buildRevision = "<unknown>"
+buildRevision = $(do
+  (exit, rev, _) <- liftIO $
+    readProcessWithExitCode "git" ["rev-parse", "HEAD"] ""
+  case exit of
+    ExitSuccess -> litE (stringL (head (lines rev)))
+    _ -> litE (stringL "<unknown>")
+  )
