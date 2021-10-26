5e76f8c5e3dd19207801ce8903ecb6d8dab28af3
Tim Rijavec <timrijavec@fb.com>
2021-10-26 05:19:56 -0700

    Added test for previous diff
    Summary: Added a unit test for testing the dependencies meta field.
    
    Reviewed By: nhawkes
    
    Differential Revision: D31897534
    
    fbshipit-source-id: 212c73d50b21c8c0945f5fbda6a6226126ca7750

---
+++ glean/test/tests/DbDependenciesTest.hs
@@ -0,0 +1,50 @@
+-- Copyright (c) Facebook, Inc. and its affiliates.
+
+module DbDependenciesTest (main) where
+
+import Control.Monad
+import Data.Default
+import qualified Data.HashMap.Strict as HashMap
+import Test.HUnit
+
+import TestRunner
+
+import Glean
+import Glean.Init
+import Glean.Database.Test
+
+repo1 :: Repo
+repo1 = Repo "dbtest-repo" "f00001"
+
+repo2 :: Repo
+repo2 = Repo "dbtest-repo" "f00002"
+
+dependenciesTest :: Test
+dependenciesTest = TestCase $ withTestEnv [] $ \env -> do
+  let props = HashMap.fromList [("key","value")]
+
+  void $ kickOffDatabase env def
+    { kickOff_repo = repo1
+    , kickOff_fill = Just (KickOffFill_writeHandle "")
+    , kickOff_properties = props
+    }
+
+  void $ completeTestDB env repo1
+
+  void $ kickOffDatabase env def
+    { kickOff_repo = repo2
+    , kickOff_dependencies = Just $ Dependencies_stacked repo1
+    , kickOff_fill = Just (KickOffFill_writeHandle "")
+    , kickOff_properties = props
+    }
+  ListDatabasesResult{..} <- listDatabases env def
+  let db = filter (\db -> database_repo db == repo2)
+            listDatabasesResult_databases
+
+  assertEqual "dependencyTest" (Just repo1) $ case db of
+    [Database{database_dependencies=Just (Dependencies_stacked d)}] -> Just d
+    _ -> Nothing
+
+main :: IO ()
+main = withUnitTest $ testRunner $ TestList
+  [ TestLabel "dependenciesTest" dependenciesTest ]
