0effbec2bf91625838d9661164480fc196540a0d
Simon Marlow <smarlow@fb.com>
2021-11-15 10:09:49 -0800

    Derive EntityByDocAttrKey in the tests
    Reviewed By: nhawkes
    
    Differential Revision: D32396426
    
    fbshipit-source-id: 6eb70411b11ca6c6922e2c44de21dbc07d829cb9

--- glean/lang/clang/tests/Glean/Regression/Driver/DocBlock.hs
+++ glean/lang/clang/tests/Glean/Regression/Driver/DocBlock.hs
@@ -2,17 +2,27 @@
 
 module Glean.Regression.Driver.DocBlock (main) where
 
+import Control.Monad
+
 import qualified Glean.Clang.Test as Clang (driver)
 import qualified Glean.DocBlock.Test as DocBlock (driver)
 import Glean.Regression.Config (Driver(..))
 import Glean.Regression.Test (testMain)
+import Glean.Derive (derivePredicate)
+import Glean.Write (parseRef)
 
 combined :: Driver
 combined = Clang.driver
   { driverGenerator = \ test backend repo -> do
       driverGenerator Clang.driver test backend repo
       driverGenerator DocBlock.driver test backend repo
+      forM_ passes $ \predicate ->
+        derivePredicate backend repo Nothing Nothing predicate Nothing
   }
+  where
+  passes = map parseRef
+    [ "docmarkup.EntityByDocAttrKey"
+    ]
 
 main :: IO ()
 main = testMain combined
