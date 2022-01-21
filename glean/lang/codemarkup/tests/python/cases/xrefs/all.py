c7ce6a426a2c8b03265c0abf8eb7c3f04b67604c
Simon Marlow <smarlow@fb.com>
2022-01-21 08:24:44 -0800

    Update license headers
    Reviewed By: zertosh
    
    Differential Revision: D33708571
    
    fbshipit-source-id: 519d35ffef276dd0ac361a411d83ea1144f86276

--- glean/lang/codemarkup/tests/python/cases/xrefs/all.py
+++ glean/lang/codemarkup/tests/python/cases/xrefs/all.py
@@ -1,4 +1,8 @@
-# (c) Facebook, Inc. and its affiliates. Confidential and proprietary.
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
 
 from big_lib import big_func, big_var
 from lib import HelperClass as Helper
