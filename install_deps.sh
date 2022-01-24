ddff400170084446f71c9b9b51cd000e1e0117e5
Don Stewart <don.stewart@tngtech.com>
2022-01-24 14:58:48 +1100

    Document some of the pitfalls of the non-getdeps.py way to build
    In particular, if you use getdeps.py we don't need to set up
    LD_LIBRARY_PATHS or have sudo access

--- install_deps.sh
+++ install_deps.sh
@@ -5,6 +5,14 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+#
+# Note, if you don't use BUILD_DEPS=1 hsthrift deps will be installed into
+# /usr/local, which requires sudo privs, and you'll need to set:
+#
+# > export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+# > export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig#
+#
+
 set -e
 
 HSTHRIFT_REPO=https://github.com/donsbot/hsthrift.git
