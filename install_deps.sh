b7ff832f13c0b82340f91ac2b8e1045355d7c369
Don Stewart <don.stewart@tngtech.com>
2022-01-31 00:58:51 +0000

    Use hsthrift non-sudo build style
    This teaches the ci and install step how to build hsthrift dependencies
    without sudo using getdeps.py. libraries and binaries will be placed
    into $HOME/.hsthrift/*
    
    Glean builds then update their paths accordingly
    
    cabal update has to run after hsthrift clone

--- install_deps.sh
+++ install_deps.sh
@@ -5,8 +5,33 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+#
+# Note, if you set GLEAN_DEPS_WITH_SUDO=1 hsthrift deps will be installed into
+# /usr/local, which requires sudo privs, and you'll need to set:
+#
+# > export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+# > export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
+#
+# You will need --sudo passed to ./install_deps.sh
+#
+# To build without root privs (the default), you will need to set:
+#
+# > export LD_LIBRARY_PATH=$HOME/.hsthrift/lib
+# > export PKG_CONFIG_PATH=$HOME/.hsthrift/lib/pkgconfig
+# > export PATH=$PATH:$HOME/.hsthrift/bin
+#
+
 set -e
 
-git clone https://github.com/facebookincubator/hsthrift.git
+HSTHRIFT_REPO=https://github.com/facebookincubator/hsthrift.git
+
+if test ! -d hsthrift; then
+    git clone "${HSTHRIFT_REPO}"
+fi
+
 cd hsthrift
-./install_deps.sh --nuke
+if test -z "${GLEAN_DEPS_WITH_SUDO}"; then
+    ./new_install_deps.sh
+else
+    ./install_deps.sh --nuke
+fi
