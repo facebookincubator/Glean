2b7c355dc36f879628359c5032f6a32e6139f9c7
Marcelo Lazaroni <lazamar@fb.com>
2022-02-11 02:41:55 -0800

    Parallelise database validation
    Summary:
    Traverse the database in parallel to speed-up validation.
    
    What takes the longest in the validation process is fetching facts from the disk.
    To speed this up we will have multiple threads, each reading from a different part of the database.
    
    The strategy here is to divide the id space in multiple sections which can be processed in parallel.
    We don't divide the entire the entire id space in a few large sections because large facts may be clustered together and this division would lead to one worker taking significantly longer than all other ones.
    
    Reviewed By: simonmar, nhawkes
    
    Differential Revision: D33943654
    
    fbshipit-source-id: 5537384b8ad30c075f8a38a2a392a2ee2242f6e0

--- glean/rts/validate.cpp
+++ glean/rts/validate.cpp
@@ -10,66 +10,139 @@
 #include "glean/rts/validate.h"
 
 #include <vector>
+#include <future>
+#include <mutex>
 
 namespace facebook {
 namespace glean {
 namespace rts {
 
+// Validates facts from startingId until firstFreeId until all items have been
+// validated or the validation limit is reached.
 void validate(const Inventory& inventory, const Validate& val, Lookup& facts) {
   const auto starting_id = facts.startingId();
   const auto first_free = facts.firstFreeId();
-  auto allowed_id = starting_id;
 
-  std::vector<Pid> types(distance(starting_id, first_free), Pid::invalid());
+  size_t elems = distance(starting_id, first_free);
+  size_t max_shards = 10000; // 0.4mb of mutexes
+  size_t lock_shard_size = std::min(elems, max_shards);
+  std::vector<std::mutex> locks(elems / lock_shard_size);
+  std::vector<Pid> types(elems, Pid::invalid());
 
-  Renamer checker([&](Id id, Pid type) {
-    auto real_type = types.at(distance(starting_id, id));
-    if (type != real_type) {
-      rts::error("fact type mismatch");
+  std::mutex err_lock;
+  std::string err;
+  std::atomic<bool> has_err = false;
+  auto fail([&](std::string msg) {
+    const std::lock_guard<std::mutex> lock(err_lock);
+    has_err.store(true);
+    err = msg;
+  });
+
+  auto has_failure([&]() {
+    return has_err.load();
+  });
+
+  // thread-safe type cache check
+  auto expect_type([&](Id id, Pid type) {
+    auto ix = distance(starting_id, id);
+    size_t lock_ix = ix / lock_shard_size;
+    const std::lock_guard<std::mutex> lock(locks.at(lock_ix));
+
+    if (types.at(ix) == Pid::invalid()) {
+      types[ix] = type;
     }
+
+    auto real_type = types.at(ix);
+    if (type != real_type) {
+      fail("fact type mismatch");
+    }
+
     return id;
   });
 
-  size_t count = 0;
+  Renamer checker(expect_type);
 
-  for (auto i = facts.enumerate(); auto fact = i->get(); i->next()) {
-    if (count >= val.limit) {
-      break;
-    }
-    ++count;
+  std::atomic<size_t> count = 0;
+  auto validate_section([&](Id from, Id to) {
+    auto allowed_id = from;
 
-    if (fact.id < allowed_id) {
-      rts::error("enumeration out of order");
-    }
-    if (fact.id >= first_free) {
-      rts::error("fact id out of bounds");
-    }
+    for (auto i = facts.enumerate(from, to); auto fact = i->get(); i->next()) {
+      if (count.load() >= val.limit || has_failure()) {
+        break;
+      }
+      ++count;
 
-    if (val.typecheck) {
-      const auto *predicate = inventory.lookupPredicate(fact.type);
-      if (predicate == nullptr) {
-        rts::error("invalid predicate");
+      if (fact.id < allowed_id) {
+        fail("enumeration out of order");
+      }
+      if (fact.id >= first_free) {
+        fail("fact id out of bounds");
       }
 
-      binary::Output out;
-      uint64_t key_size;
+      if (val.typecheck) {
+        const auto *predicate = inventory.lookupPredicate(fact.type);
+        if (predicate == nullptr) {
+          fail("invalid predicate");
+        }
 
-      predicate->typecheck(checker, fact.clause, out, key_size);
+        binary::Output out;
+        uint64_t key_size;
 
-      if (fact.clause.bytes() != out.bytes()) {
-        rts::error("invalid fact");
+        predicate->typecheck(checker, fact.clause, out, key_size);
+
+        if (fact.clause.bytes() != out.bytes()) {
+          fail("invalid fact");
+        }
+        if (fact.clause.key_size != key_size) {
+          fail("key size mismatch");
+        }
       }
-      if (fact.clause.key_size != key_size) {
-        rts::error("key size mismatch");
+
+      if (val.keys && facts.idByKey(fact.type, fact.key()) != fact.id) {
+        fail("idByKey mismatch");
       }
-    }
 
-    if (val.keys && facts.idByKey(fact.type, fact.key()) != fact.id) {
-      rts::error("idByKey mismatch");
-    }
+      allowed_id = fact.id + 1;
 
-    allowed_id = fact.id + 1;
-    types[distance(starting_id, fact.id)] = fact.type;
+      expect_type(fact.id, fact.type);
+    }
+  });
+
+  const auto max_parallel = 10;
+  size_t min_ids_per_section = 100000;
+  auto ids_per_section = std::min(min_ids_per_section, elems / max_parallel);
+  std::atomic<int> section_no = 0;
+  auto get_next_section_starting_id([&]() {
+    int section = section_no++;
+    return starting_id + section * ids_per_section;
+  });
+
+  // validate multiple sections
+  auto worker([&]() {
+    while (true) {
+      Id from = get_next_section_starting_id();
+      Id to = std::min(from + ids_per_section, first_free);
+
+      if (from >= first_free || count.load() >= val.limit || has_failure()) {
+        break;
+      }
+
+      validate_section(from, to);
+    }
+  });
+
+  std::vector<std::future<void>> workers(max_parallel);
+  // start workers
+  for (auto i = 0; i < max_parallel; i++) {
+    workers[i] = std::async(worker);
+  }
+
+  for (auto& w : workers) {
+    w.wait();
+  }
+
+  if (has_failure()) {
+    rts::error(err);
   }
 }
 
