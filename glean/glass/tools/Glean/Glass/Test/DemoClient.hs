567fe2bedb19f0a7f3e7cb61c5ea3c3172ef3a20
Don Stewart <don.stewart@tngtech.com>
2022-02-17 02:30:05 +0000

    A simple demo of a native Glass client
    - lists symbol ids in a file, talkign to the glass thrift server over cpp-channels
    - only depends on glass, not glean stuff.
    
    defaults are to query the demo react index
    
    Example:
    ```
    $ cabal build glass-democlient
    $ glass-democlient
    react/js/ReactHooks.js/src/react/packages/react/useCacheRefresh
    react/js/ReactHooks.js/src/react/packages/react/useCallback
    react/js/ReactHooks.js/src/react/packages/react/useContext
    react/js/ReactHooks.js/src/react/packages/react/useDebugValue
    react/js/ReactHooks.js/src/react/packages/react/useDeferredValue
    react/js/ReactHooks.js/src/react/packages/react/useEffect
    react/js/ReactHooks.js/src/react/packages/react/useId
    ```
    
    Those symbol ids can go into glass-symbol, which is a fat client of glass-lib:
    ```
    $ glass-symbol --service localhost:35187 -c location -s react/js/react/packages/react/src/ReactHooks.js/useEffect
    OK 10.50ms react/js/react/packages/react/src/ReactHooks.js/useEffect -> react@react/packages/react/src/ReactHooks.js 3399:(3399+9)
    ```
    
    I note the JS symbol-id using node-style modules is a reversed path (bug)

---
+++ glean/glass/tools/Glean/Glass/Test/DemoClient.hs
@@ -0,0 +1,77 @@
+{-# LANGUAGE ApplicativeDo #-}
+{-
+  Copyright (c) Meta Platforms, Inc. and affiliates.
+  All rights reserved.
+
+  This source code is licensed under the BSD-style license found in the
+  LICENSE file in the root directory of this source tree.
+-}
+
+module Glean.Glass.Test.DemoClient (main) where
+
+import Glean.Init as Glean ( withOptions )
+
+import Glean.Glass.Types
+import Glean.Glass.GlassService.Client
+    ( GlassService, documentSymbolListX )
+
+import Options.Applicative
+import Data.Text (Text)
+import qualified Data.Set as Set
+import qualified Data.Text as Text
+import qualified Data.Text.IO as Text
+import Data.Default ( Default(def) )
+import Control.Monad.Trans ( MonadIO(liftIO) )
+
+import Util.OptParse ( maybeTextOption, maybeIntOption )
+
+import Thrift.Channel.HeaderChannel
+    ( HeaderConfig(..), withHeaderChannel )
+import Thrift.Protocol.Id ( compactProtocolId )
+import Util.EventBase ( withEventBaseDataplane )
+
+newtype Config = Config { cfgFile :: Maybe Text }
+
+options :: ParserInfo Config
+options = info (helper <*> parser) fullDesc
+  where
+    parser :: Parser Config
+    parser = do
+      cfgFile <- maybeTextOption (long "file" <> short 'f' <> metavar "FILE")
+      return Config{..}
+
+defQuery :: DocumentSymbolsRequest
+defQuery = def {
+  documentSymbolsRequest_repository = RepoName "react",
+  documentSymbolsRequest_filepath =
+    Path "react/packages/react/src/ReactHooks.js",
+  documentSymbolsRequest_include_refs = True
+}
+
+defCfg :: HeaderConfig GlassService
+defCfg = HeaderConfig
+  { headerHost = "127.0.0.1"
+  , headerPort = 26073
+  , headerProtocolId = compactProtocolId
+  , headerConnTimeout = 5000
+  , headerSendTimeout = 5000
+  , headerRecvTimeout = 5000
+  }
+
+main :: IO ()
+main = Glean.withOptions options $ \Config{..} -> do
+  let query = case cfgFile of
+        Nothing -> defQuery
+        Just f -> defQuery { documentSymbolsRequest_filepath = Path f }
+  withEventBaseDataplane $ \evp ->
+    withHeaderChannel evp defCfg $ do
+      DocumentSymbolListXResult{..} <- documentSymbolListX query def
+      liftIO $ mapM_ Text.putStrLn $
+        pprDefs documentSymbolListXResult_definitions
+
+-- List unique definition symbols
+pprDefs :: [DefinitionSymbolX] -> [Text]
+pprDefs = Set.toList . Set.fromList . map pprDef
+  where
+    pprDef :: DefinitionSymbolX -> Text
+    pprDef DefinitionSymbolX{..} = unSymbolId definitionSymbolX_sym
