9aea4acf873a62cd134694d6534f8047b222f7cd
Alper Yoney <alperyoney@fb.com>
2022-02-23 00:22:13 -0800

    implement min_db_age in glean server-side
    Summary: Implementation of minimum age of a DB before we consider listing with `GleanService.listDatabases()`. The idea here is that if we delay the listing, there's a better chance that the DB will have propagated to all the servers.
    
    Reviewed By: simonmar
    
    Differential Revision: D34382387
    
    fbshipit-source-id: 647ee9a328799c78f6d249ccca43e7b034799bf8

--- glean/db/Glean/Database/List.hs
+++ glean/db/Glean/Database/List.hs
@@ -16,6 +16,7 @@ import Data.HashMap.Strict (HashMap)
 import qualified Data.HashMap.Strict as HashMap
 import Data.Maybe
 import Data.Text (Text)
+import Data.Time.Clock (getCurrentTime)
 
 import Util.Control.Exception
 import Util.Log
@@ -25,12 +26,17 @@ import qualified Glean.Database.Backup.Locator as Backup
 import qualified Glean.Database.Catalog as Catalog
 import Glean.Database.Meta
 import Glean.Database.Types
+import qualified Glean.ServerConfig.Types as ServerConfig
 import Glean.Types hiding (Database)
 import qualified Glean.Types as Thrift
+import qualified Glean.Util.Observed as Observed
 
 
 listDatabases :: Env -> Thrift.ListDatabases -> IO Thrift.ListDatabasesResult
 listDatabases env@Env{..} Thrift.ListDatabases{..} = do
+  now <- utcTimeToPosixEpochTime <$> getCurrentTime
+  minDBAge <- ServerConfig.config_min_db_age <$> Observed.get envServerConfig
+  let filterWithMinDBAge = filterDatabasePred now minDBAge
   backups <-
     if listDatabases_includeBackups
       then do
@@ -41,13 +47,11 @@ listDatabases env@Env{..} Thrift.ListDatabases{..} = do
       else
         return mempty
   local <- atomically $ Catalog.getLocalDatabases envCatalog
+  let databases = mapToDatabase $ HashMap.elems $ HashMap.union local backups
   return Thrift.ListDatabasesResult
-    { listDatabasesResult_databases =
-        map Thrift.getDatabaseResult_database
-        $ HashMap.elems
-        $ HashMap.union local backups
-    }
+    { listDatabasesResult_databases = filter filterWithMinDBAge databases }
   where
+    mapToDatabase = map Thrift.getDatabaseResult_database
     reposToResults = HashMap.mapWithKey
       (\repo meta -> Thrift.GetDatabaseResult
         { getDatabaseResult_database = metaToThriftDatabase
@@ -57,6 +61,12 @@ listDatabases env@Env{..} Thrift.ListDatabases{..} = do
             meta
         , getDatabaseResult_tasks = Nothing
         })
+    filterDatabasePred now minDBAge db = currentAgeInSeconds > minDBAge
+      where
+        dbTime = case Thrift.database_completed db of
+          Just completedAt -> completedAt
+          Nothing -> Thrift.database_created_since_epoch db
+        currentAgeInSeconds = unPosixEpochTime now - unPosixEpochTime dbTime
 
 listRestorable :: Backup.Site site => Text -> site -> IO (HashMap Repo Meta)
 listRestorable prefix site =
