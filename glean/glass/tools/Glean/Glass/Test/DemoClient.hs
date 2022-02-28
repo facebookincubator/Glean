3214ea2efe146dae937298ba99c4c2c9606b4fe8
Don Stewart <don.stewart@tngtech.com>
2022-02-28 06:40:16 +0000

    Refactor demo-client and implement 'resolve'
    ```
    > glass-democlient resolve react/js/react/packages/react/src/ReactHooks.js/useCallback
    react@packages/react/src/ReactHooks.js:124:17-124:28
    ```
    
    and add --service option

--- glean/glass/tools/Glean/Glass/Test/DemoClient.hs
+++ glean/glass/tools/Glean/Glass/Test/DemoClient.hs
@@ -7,75 +7,109 @@
 -}
 
 {-# LANGUAGE ApplicativeDo #-}
-module Glean.Glass.Test.DemoClient (main) where
+module Glean.Glass.Test.DemoClient ( main ) where
 
 import Glean.Init as Glean ( withOptions )
 
 import Glean.Glass.Types
 import Glean.Glass.GlassService.Client
-    ( documentSymbolListX,
-      GlassService,
-      findReferenceRanges,
-      describeSymbol )
 
+import Data.Default ( Default(def) )
+import Data.Maybe
+import Data.Text (Text)
 import Options.Applicative
 import Options.Applicative.Types ( readerAsk )
-import Data.Text (Text)
+import System.FilePath ( pathSeparator )
+import Util.Text ( textShow, textToInt )
+import Text.Read ( readMaybe )
 import qualified Data.Text as Text
 import qualified Data.Text.IO as Text
-import Data.Default ( Default(def) )
-import Control.Monad.Trans ( MonadIO(liftIO) )
-import System.FilePath (pathSeparator)
-import Util.Text ( textShow )
 
-import Thrift.Channel.HeaderChannel ( HeaderConfig(..), withHeaderChannel )
+import Thrift.Monad as Thrift
+import Thrift.Protocol
+import Thrift.Protocol.Compact
+import Thrift.Channel.HeaderChannel
 import Thrift.Protocol.Id ( compactProtocolId )
+
 import Util.EventBase ( withEventBaseDataplane )
 import Control.Monad.Extra (whenJust)
-newtype Options = Options { optCommand :: Command }
+
+data Options =
+  Options {
+    optCommand :: Command,
+    optHost :: Service
+  }
+
+data Service =
+  Service {
+    svcHost :: Text,
+    svcPort :: Int
+  }
+
+defService :: Service
+defService = Service "127.0.0.1" 26073
 
 data Command
   = List RepoName Path
   | Describe SymbolId
   | FindRefs SymbolId
+  | Resolve SymbolId
 
 options :: ParserInfo Options
-options = info (helper <*> parser) fullDesc
+options = info (helper <*> parser) (fullDesc <>
+    progDesc "Glass code navigation and search tool")
   where
     parser :: Parser Options
-    parser = hsubparser $
-      command "list" (info listCommand
-        (progDesc $ unlines
-          ["List symbols in file specified by REPO/PATH"
-          ," e.g. glass list react/packages/react/src/ReactHooks.js"
-          ]
+    parser = do
+      optHost <- option readService
+        (value defService
+        <> long "service"
+        <> metavar "HOST:PORT"
         )
-      ) <>
-      command "describe" (info describeCommand
-        (progDesc $ unlines
-          ["Describe basic details of a symbol"
-          ," e.g. glass describe react/js/ReactHooks.js/src/react/packages/react/useEffect"
-          ]
+      optCommand <- hsubparser $
+        command "list" (info listCommand
+          (progDesc $ unlines
+            ["List symbols in file specified by REPO/PATH"
+            ]
+          )
+        ) <>
+        command "describe" (info describeCommand
+          (progDesc $ unlines
+            ["Describe basic details of a symbol"
+            ]
+          )
+        ) <>
+        command "references" (info findRefsCommand
+          (progDesc $ unlines
+            ["Find references to this symbol"
+            ]
+          )
+        ) <>
+        command "resolve" (info resolveCommand
+          (progDesc $ unlines
+            ["Find the definition location of a symbol"
+            ]
+          )
         )
-      ) <>
-      command "references" (info findRefsCommand
-        (progDesc $ unlines
-          ["Find references to this symbol"
-          ," e.g. glass references react/js/ReactHooks.js/src/react/packages/react/useEffect"
-          ]
-        )
-      )
+      return $ Options {..}
 
-    listCommand :: Parser Options
-    listCommand = Options <$> argument readListRepoPath (metavar "REPO/PATH")
+    cmd parser help = argument parser help
 
-    describeCommand :: Parser Options
-    describeCommand = Options <$> argument
-      (Describe <$> readSymbol) (metavar "SYMBOL")
+    symbolHelp = metavar "SYMBOL"
 
-    findRefsCommand :: Parser Options
-    findRefsCommand = Options <$> argument
-      (FindRefs <$> readSymbol) (metavar "SYMBOL")
+    listCommand = cmd readListRepoPath (metavar "REPO/PATH")
+    describeCommand = cmd (Describe <$> readSymbol) symbolHelp
+    findRefsCommand = cmd (FindRefs <$> readSymbol) symbolHelp
+    resolveCommand = cmd (Resolve <$> readSymbol) symbolHelp
+
+readService :: ReadM Service
+readService = do
+  hostPort <- Text.pack <$> readerAsk
+  case Text.breakOn ":" hostPort of
+    (_, "") -> fail "Not a valid host:port"
+    (host, portStr) -> case textToInt portStr of
+      Left _ -> fail ("Not a valid port: " <> show portStr)
+      Right port -> return (Service host port)
 
 readListRepoPath :: ReadM Command
 readListRepoPath = do
@@ -114,53 +148,67 @@ defCfg = HeaderConfig
   , headerRecvTimeout = 15000
   }
 
-main :: IO ()
-main = Glean.withOptions options $ \Options{..} -> do
-  case optCommand of
-    List repo path -> runListSymbols repo path
-    Describe sym -> runDescribe sym
-    FindRefs sym -> runFindRefs sym
+type GlassM p a = ThriftM p HeaderWrappedChannel GlassService a
 
-runListSymbols :: RepoName -> Path -> IO ()
-runListSymbols repo path =
-  withEventBaseDataplane $ \evp -> withHeaderChannel evp defCfg $ do
-    DocumentSymbolListXResult{..} <- documentSymbolListX query def
-    liftIO $ mapM_ Text.putStrLn $
-      pprDefs documentSymbolListXResult_definitions
+main :: IO ()
+main = Glean.withOptions options $ \Options{..} ->
+  withEventBaseDataplane $ \evp -> do
+    res <- withHeaderChannel evp defCfg $ case optCommand of
+              List repo path -> runListSymbols repo path
+              Describe sym -> runDescribe sym
+              FindRefs sym -> runFindRefs sym
+              Resolve sym -> runResolve sym
+    mapM_ Text.putStrLn res
+
+runListSymbols :: Protocol p => RepoName -> Path -> GlassM p [Text]
+runListSymbols repo path = do
+  DocumentSymbolListXResult{..} <- documentSymbolListX query def
+  return $ pprDefs documentSymbolListXResult_definitions
   where
     query = defDocumentSymbolsReq {
             documentSymbolsRequest_repository = repo
           , documentSymbolsRequest_filepath = path
     }
 
-runDescribe :: SymbolId -> IO ()
-runDescribe sym =
-  withEventBaseDataplane $ \evp -> withHeaderChannel evp defCfg $ do
-    SymbolDescription{..} <- describeSymbol sym def
-    liftIO $ Text.putStrLn $ pprSymbolPath symbolDescription_location
-    liftIO $ Text.putStrLn $ pprQName symbolDescription_name
-    whenJust symbolDescription_kind $ liftIO . print
+runDescribe :: Protocol p => SymbolId -> GlassM p [Text]
+runDescribe sym = do
+  SymbolDescription{..} <- describeSymbol sym def
+  let loc = pprLocationRange $ toLocationRange symbolDescription_location
+      name = pprQName symbolDescription_name
+      kind = textShow <$> symbolDescription_kind
+      annot = textShow <$> symbolDescription_annotations
+  return $ [loc,name] <> catMaybes [kind,annot]
 
-runFindRefs :: SymbolId -> IO ()
-runFindRefs sym =
-  withEventBaseDataplane $ \evp -> withHeaderChannel evp defCfg $ do
-    ranges <- findReferenceRanges sym def
-    liftIO $ Text.putStrLn $
-      "Found " <> textShow (length ranges) <> " references"
+runResolve :: Protocol p => SymbolId -> GlassM p [Text]
+runResolve sym = do
+  loc@Location{..} <- resolveSymbol sym def
+  range <- jumpTo loc def
+  return
+    [pprLocationRange LocationRange {
+          locationRange_repository = location_repository,
+          locationRange_filepath = location_filepath,
+          locationRange_range = range
+        }
+    ]
+
+runFindRefs :: Protocol p => SymbolId -> GlassM p [Text]
+runFindRefs sym = do
+  ranges <- findReferenceRanges sym def
+  return $
+    ("Found " <> textShow (length ranges) <> " references") :
+    map pprLocationRange ranges
 
 -- List unique definition symbols
 pprDefs :: [DefinitionSymbolX] -> [Text]
-pprDefs = map pprDef
-  where
-    pprDef :: DefinitionSymbolX -> Text
-    pprDef DefinitionSymbolX{..} = unSymbolId definitionSymbolX_sym
+pprDefs = map (unSymbolId . definitionSymbolX_sym)
 
-pprSymbolPath :: SymbolPath -> Text
-pprSymbolPath SymbolPath{..} = Text.concat [repo, "@", path,":", pprRange range]
+pprLocationRange :: LocationRange -> Text
+pprLocationRange LocationRange{..} =
+   Text.concat [repo, "@", path,":", pprRange range]
   where
-    RepoName repo = symbolPath_repository
-    Path path = symbolPath_filepath
-    range = symbolPath_range
+    RepoName repo = locationRange_repository
+    Path path = locationRange_filepath
+    range = locationRange_range
 
 pprRange :: Range -> Text
 pprRange (Range lineBegin colBegin lineEnd colEnd) = Text.concat [
@@ -171,3 +219,10 @@ pprRange (Range lineBegin colBegin lineEnd colEnd) = Text.concat [
 pprQName :: QualifiedName -> Text
 pprQName QualifiedName{..} =
   unName qualifiedName_container <> ":" <> unName qualifiedName_localName
+
+toLocationRange :: SymbolPath -> LocationRange
+toLocationRange SymbolPath{..} = LocationRange {
+    locationRange_repository = symbolPath_repository,
+    locationRange_filepath = symbolPath_filepath,
+    locationRange_range = symbolPath_range
+  }
