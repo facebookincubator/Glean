f64ea89c489d24305ae9f843b380139f6f7d5d1c
Don Stewart <don.stewart@tngtech.com>
2022-03-30 03:49:45 -0700

    Add minimal test case for the raiseError/cxx exception catcher (#121)
    Summary:
    Since this has come up twice now on different, more exotic builds, we
    want a standalone test that passes, rather than triaging AngleTest.hs
    each time. See https://github.com/facebookincubator/Glean/issues/119 and https://github.com/facebookincubator/Glean/issues/79 for prior instances. There's something lurking here, but changing up the OS or compiler versions tends to make it go away sadly.
    
    On mis-compiled stacks, this will segfault or sigabrt. E.g.
    
    ```
        fmt=fmt@entry=0x7fd0b9f67298 "%s\n") at ../sysdeps/posix/libc_fatal.c:155
        str=str@entry=0x7fd0b9f69670 "double free or corruption (out)") at malloc.c:5347
        have_lock=<optimized out>) at malloc.c:4314
        at rts/Capability.c:601
    ```
    
    Or
    ```
    #0  __GI_raise (sig=sig@entry=6) at ../sysdeps/unix/sysv/linux/raise.c:50
    https://github.com/facebookincubator/Glean/issues/1  0x00007f7f7c7fc859 in __GI_abort () at abort.c:79
    https://github.com/facebookincubator/Glean/issues/2  0x00007f7f7c86729e in __libc_message (action=action@entry=do_abort,
        fmt=fmt@entry=0x7f7f7c991298 "%s\n") at ../sysdeps/posix/libc_fatal.c:155
    https://github.com/facebookincubator/Glean/issues/3  0x00007f7f7c86f32c in malloc_printerr (
        str=str@entry=0x7f7f7c993670 "double free or corruption (out)") at malloc.c:5347
    https://github.com/facebookincubator/Glean/issues/4  0x00007f7f7c870fd0 in _int_free (av=0x7f7f7c9c6b80 <main_arena>, p=0x7ffe1eca4b70,
        have_lock=<optimized out>) at malloc.c:4314
    https://github.com/facebookincubator/Glean/issues/5  0x0000000000f7a376 in glean_query_execute_compiled ()
    https://github.com/facebookincubator/Glean/issues/6  0x00000000021b20b5 in releaseCapability_ (cap=0x3eff9c0, always_wakeup=false)
        at rts/Capability.c:601
    https://github.com/facebookincubator/Glean/issues/7  0x0000000000e098ea in ?? ()
    https://github.com/facebookincubator/Glean/issues/8  0x0000000000000000 in ?? ()
    (gdb)
    ```
    
    Pull Request resolved: https://github.com/facebookincubator/Glean/pull/121
    
    Reviewed By: nhawkes
    
    Differential Revision: D35001528
    
    Pulled By: phlalx
    
    fbshipit-source-id: 0a017030c7209c76bdabcc23dd1b652d7fa40511

---
+++ glean/test/tests/CppCatchTest.hs
@@ -0,0 +1,58 @@
+{-
+  Copyright (c) Meta Platforms, Inc. and affiliates.
+  All rights reserved.
+
+  This source code is licensed under the BSD-style license found in the
+  LICENSE file in the root directory of this source tree.
+-}
+
+{-# LANGUAGE TypeApplications #-}
+module CppCatchTest (main) where
+
+import Control.Exception
+import qualified Data.Text as Text
+import Test.HUnit
+
+import Glean.Init
+import Glean.Query.Thrift as Thrift
+import qualified Glean.Schema.Cxx1.Types as Cxx
+import Glean.Typed hiding (end)
+import Util.Text ( textShow )
+
+import TestRunner
+import TestDB
+
+catchTest :: Test
+catchTest = dbTestCase $ \env repo -> do
+
+  -- Pick some fact ids of type Cxx.Name
+  names <- runQuery_ env repo $ allFacts @Cxx.Name
+  let factId x = Text.pack (show (fromFid (idOf (getId x))))
+
+  --
+  -- Now query for it but assert the wrong type
+  -- This should raise an exception if all is working properly
+  --
+  r <- try $ runQuery_ env repo $ angle @Cxx.Type $
+      "$cxx1.Type " <> factId (head names)
+
+  -- on mis-compiled hosts this is expected to SIGABRT, instead of
+  -- catching the C++ exception
+  case r of
+    Left (e :: SomeException) -> assertBool
+       "Correctly caught exception" ("Stack trace" `Text.isInfixOf` textShow e)
+    Right _ -> assertFailure "catchTest did not throw"
+
+
+{-
+Correct trace looks like:
+
+Stack trace:
+  facebook::glean::rts::raiseError(std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > const&)
+  std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > facebook::glean::rts::error<>(folly::Range<char const*>)
+  facebook::glean::rts::Subroutine::execute(unsigned long const*) const
+-}
+
+main :: IO ()
+main = withUnitTest $ testRunner $
+  TestLabel "catch from C++" catchTest
