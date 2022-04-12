c9644d7f35fcf0463e3572ace206d6fbfa588222
Michael Park <mcypark@fb.com>
2022-04-12 09:35:49 -0700

    Add tests involving builtin entities.
    Summary: This diff adds a few tests that involve built-in entities such as `__int128_t` and global `operator new`.
    
    Reviewed By: simonmar
    
    Differential Revision: D35517278
    
    fbshipit-source-id: 8e9e35bd474be17e1e18f55d2cafad49e7ee8d46

---
+++ glean/lang/clang/tests/regression/declarations/builtin1/test.cpp
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+using I = __int128_t;
+
+void f() {
+    ::new I;
+}
