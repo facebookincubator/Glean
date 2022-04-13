61044f554c5c457173c959e1241edce7b3fa48bf
Simon Marlow <smarlow@fb.com>
2022-04-13 08:21:17 -0700

    Add Python regression tests
    Reviewed By: nhawkes
    
    Differential Revision: D35582846
    
    fbshipit-source-id: 59d4504cd3a8027f1313758ae5826937a9580df5

--- glean/glass/test/regression/Glean/Glass/Regression/Python.hs
+++ glean/glass/test/regression/Glean/Glass/Regression/Python.hs
@@ -8,9 +8,13 @@
 
 module Glean.Glass.Regression.Python (main) where
 
+import Data.Bifunctor
+import Data.Text (Text)
 import Test.HUnit
 
+import Glean
 import Glean.Regression.Test
+import Glean.Util.Some
 
 import Glean.Glass.Types
 import Glean.Glass.Regression.Tests
@@ -18,4 +22,75 @@ import Glean.Glass.Regression.Tests
 main :: IO ()
 main = mainTestIndexExternal "glass-regression-python" $ \get -> TestList
   [ testDocumentSymbolListX (Path "all.py") get
+  , testSymbolIdLookup get
+  , testPythonFindReferences get
+  -- Python doesn't support comments yet, so no testDescribeSymbolComments
   ]
+
+testSymbolIdLookup :: IO (Some Backend, Repo) -> Test
+testSymbolIdLookup get = TestLabel "describeSymbol" $ TestList [
+  -- class
+  "test/py/big_lib.big_func" --> "big_lib/__init__.py",
+  "test/py/big_lib.big_var" --> "big_lib/__init__.py",
+  "test/py/lib.HelperClass" --> "lib.py",
+  "test/py/lib.HelperClass.value" --> "lib.py",
+  "test/py/lib.HelperClass.method" --> "lib.py",
+  "test/py/lib.HelperClass.another_method" --> "lib.py",
+  "test/py/lib.helper_func" --> "lib.py",
+  "test/py/main.f" --> "main.py",
+  "test/py/main.g" --> "main.py",
+  "test/py/shadow.ShadowClass" --> "shadow.py",
+  "test/py/shadow.ShadowClass.big_func" --> "shadow.py",
+  "test/py/shadow.ShadowClass.other_var" --> "shadow.py",
+  "test/py/shadow.local_var" --> "shadow.py",
+  "test/py/shadow.big_func" --> "shadow.py",
+  "test/py/shadow.other_var" --> "shadow.py",
+  "test/py/all.LocalClass" --> "all.py"
+  ]
+  where
+    (-->) :: Text -> Text -> Test
+    sym --> expected =
+      testDescribeSymbolMatchesPath
+        (SymbolId sym)
+        (Path expected)
+        get
+
+testPythonFindReferences :: IO (Some Backend, Repo) -> Test
+testPythonFindReferences get = TestLabel "findReferences" $ TestList [
+  "test/py/big_lib.big_func" --> [
+    ("all.py",2),
+    ("big_lib/relative.py",2),
+    ("lib.py",1),
+    ("main.py",2),
+    ("shadow.py",2) ],
+  "test/py/big_lib.big_var" --> [
+    ("all.py",3),
+    ("as.py",1),
+    ("big_lib/__init__.py",1),
+    ("shadow.py",4) ],
+  "test/py/lib.HelperClass" --> [
+    ("all.py",4),
+    ("as.py",1),
+    ("lib.py",5),
+    ("main.py",4) ],
+  "test/py/lib.HelperClass.value" --> [],
+  "test/py/lib.HelperClass.method" --> [],
+  "test/py/lib.HelperClass.another_method" --> [],
+  "test/py/lib.helper_func" --> [("big_lib/relative.py",1), ("main.py",2)],
+  "test/py/main.f" --> [("main.py",1)],
+  "test/py/main.g" --> [],
+  "test/py/shadow.ShadowClass" --> [],
+  "test/py/shadow.ShadowClass.big_func" --> [("shadow.py",1)],
+  "test/py/shadow.ShadowClass.other_var" --> [],
+  "test/py/shadow.local_var" --> [("shadow.py",1)],
+  "test/py/shadow.big_func" --> [("shadow.py",1)],
+  "test/py/shadow.other_var" --> [],
+  "test/py/all.LocalClass" --> [("all.py",1)]
+  ]
+  where
+    (-->) :: Text -> [(Text,Int)] -> Test
+    sym --> expected =
+      testFindReferences
+        (SymbolId sym)
+        (map (first Path) expected)
+        get
