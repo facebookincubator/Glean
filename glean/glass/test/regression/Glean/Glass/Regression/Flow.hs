e253fb114439088b6cb410a455c2e9aa940efda1
Srinath Vijayaragavan <srinathvijay@fb.com>
2022-04-19 04:05:25 -0700

    Added symbolId tests for Flow
    Summary: Diff contains testcases to resolve SymbolIds for Flow/Javascript symbols to the correct file. The sample Flow code to refer symbols is found in /fbsource/fbcode/glean/lang/codemarkup/tests/flow/cases/xrefs
    
    Reviewed By: simonmar
    
    Differential Revision: D35642932
    
    fbshipit-source-id: c152861c2e2169fc97b0b4bd483b980cf7b9a81f

--- glean/glass/test/regression/Glean/Glass/Regression/Flow.hs
+++ glean/glass/test/regression/Glean/Glass/Regression/Flow.hs
@@ -9,13 +9,40 @@
 module Glean.Glass.Regression.Flow (main) where
 
 import Test.HUnit
+import Data.Text (Text)
 
 import Glean.Regression.Test
 
+import Glean
+import Glean.Util.Some
 import Glean.Glass.Types
 import Glean.Glass.Regression.Tests
 
 main :: IO ()
 main = mainTestIndexExternal "glass-regression-flow" $ \get -> TestList
   [ testDocumentSymbolListX (Path "test/imports.js") get
+  , testSymbolIdLookup get
   ]
+
+testSymbolIdLookup :: IO (Some Backend, Repo) -> Test
+testSymbolIdLookup get = TestLabel "describeSymbol" $ TestList [
+  "test/js/test/es_exports.js.flow/foo" --> "test/es_exports.js.flow",
+  "test/js/test/es_exports.js.flow/bor" --> "test/es_exports.js.flow",
+  "test/js/test/es_exports.js.flow/d" --> "test/es_exports.js.flow",
+  "test/js/test/es_exports.js.flow/C" --> "test/es_exports.js.flow",
+  "test/js/test/es_exports.js.flow/num" --> "test/es_exports.js.flow",
+  "test/js/test/cjs_exports.js/plus" --> "test/cjs_exports.js",
+  "test/js/test/imports.js/a" --> "test/imports.js",
+  "test/js/test/imports.js/caz" --> "test/imports.js",
+  "test/js/test/imports.js/s" --> "test/imports.js",
+  "test/js/test/imports.js/qux" --> "test/imports.js",
+  "test/js/test/imports.js/str" --> "test/imports.js",
+  "test/js/test/imports.js/fn" --> "test/imports.js"
+  ]
+  where
+    (-->) :: Text -> Text -> Test
+    sym --> expected =
+      testDescribeSymbolMatchesPath
+        (SymbolId sym)
+        (Path expected)
+        get
