53162a70b778458aa786cf104f3ffec297ca6a11
Simon Marlow <smarlow@fb.com>
2022-04-21 13:41:13 -0700

    Migrate from existing .bzl logic to Indexer for regression tests
    Summary:
    Finally, removing the duplication by using the new Indexer abstraction
    instead of the .bzl logic. This makes the indexers usable in the open
    source build without having to duplicate the logic there.
    
    Reviewed By: pepeiborra
    
    Differential Revision: D35814034
    
    fbshipit-source-id: e74deaf7c4d1af684cd0e00d73b45eb5e41474be

--- glean/test/regression/Glean/Regression/Driver/External.hs
+++ glean/test/regression/Glean/Regression/Driver/External.hs
@@ -10,8 +10,29 @@
 
 module Glean.Regression.Driver.External (main) where
 
-import Glean.Regression.Snapshot
+import System.Environment
+
+import Util.Log
+
+import Glean.Indexer
+import Glean.Indexer.List (cmdLineParser)
+
+import Glean.Regression.Snapshot (testMain)
 import Glean.Regression.Snapshot.Driver
 
+snapshotDriver :: Driver RunIndexer
+snapshotDriver = driverFromIndexer
+  Indexer {
+    indexerShortName = "multiple",
+    indexerDescription = "Choose an indexer to run",
+    indexerOptParser = cmdLineParser,
+    indexerRun = id
+  }
+
 main :: IO ()
-main = testMain externalDriver
+main = do
+  args <- getArgs
+  logInfo $ "args: " <> show args
+    -- useful for debugging issues in surrounding scripts that pass
+    -- arguments in.
+  testMain snapshotDriver
