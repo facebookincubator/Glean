9499149333c5c445b619a1fa96103acd358c33a0
Pepe Iborra <pepeiborra@fb.com>
2022-05-16 09:17:00 -0700

    Fix --relocatable-db with --dont-create-db
    Summary: The new testsuite uses both options, and it turns out that they are currently incompatible, since ``dont-create-db` copies the `hiedb` file to a temporary directory breaking any relative paths it contains
    
    Reviewed By: simonmar
    
    Differential Revision: D36280585
    
    fbshipit-source-id: a0315260d03b0a70ed8b50fad3a183fa38bcf244

--- glean/lang/haskell/HieDBIndexer/Main.hs
+++ glean/lang/haskell/HieDBIndexer/Main.hs
@@ -5,6 +5,7 @@
   This source code is licensed under the BSD-style license found in the
   LICENSE file in the root directory of this source tree.
 -}
+
 {-# LANGUAGE OverloadedStrings #-}
 
 module HieDBIndexer.Main where
@@ -24,41 +25,39 @@ import HieDBIndexer.Options (
  )
 import HieDBIndexer.Trace (traceMsg, vlogTextTracer)
 import System.Directory (copyFile)
-import System.IO.Temp (createTempDirectory)
+import System.IO.Temp (withTempFile)
+import System.IO
+import System.FilePath (takeDirectory)
 import Util.EventBase (withEventBaseDataplane)
 
 {- | Tests run concurrently, and they become flaky because the
  HieDB gets locked when running a test. This copies the DB to a temp dir
  before running a test.
 -}
-copyHieDbToTempDir ::
-  HieDBIndexerOptions ->
-  IO HieDBIndexerOptions
-copyHieDbToTempDir opts@HieDBIndexerOptions {..} = do
-  hiedbTempDir <- createTempDirectory "/tmp" "hiedb_temp"
-  let finalDir = hiedbTempDir <> "hiedb"
-  copyFile hiedbPath finalDir
-  return opts {hiedbPath = finalDir}
+handleDontCreateDb :: HieDBIndexerOptions -> (HieDBIndexerOptions -> IO a) -> IO a
+handleDontCreateDb opts k
+  | dontCreateDb opts = do
+      withTempFile (takeDirectory $ hiedbPath opts) "hiedb_temp" $ \fp h -> do
+        hClose h
+        copyFile (hiedbPath opts) fp
+        k opts {hiedbPath = fp}
+  | otherwise = k opts
 
 main :: IO ()
 main = do
   let tracer = vlogTextTracer 0
   withOptions options $ \cfg ->
-    withEventBaseDataplane $ \evb ->
-      withConfigProvider defaultConfigOptions $ \cfgAPI ->
-        Glean.withBackendWithDefaultOptions evb cfgAPI (cfgService cfg) $
-          \backend -> do
-            finalCfg <-
-              if dontCreateDb cfg
-                then -- dontCreateDb is True when running the regression tests.
-                  copyHieDbToTempDir cfg
-                else return cfg
-            (fileLinesMap, xrefMapData) <-
-              traceMsg tracer "buildXrefMapFiles" $
-                buildXrefMapFiles tracer finalCfg
-            putStrLn "Finished creating the data and saving to files."
-            traceMsg tracer "createGleanDB" $
-              createGleanDB
-                (HieDBIndexerEnv backend finalCfg)
-                fileLinesMap
-                xrefMapData
+    handleDontCreateDb cfg $ \finalCfg -> do
+      withEventBaseDataplane $ \evb ->
+        withConfigProvider defaultConfigOptions $ \cfgAPI ->
+          Glean.withBackendWithDefaultOptions evb cfgAPI (cfgService cfg) $
+            \backend -> do
+              (fileLinesMap, xrefMapData) <-
+                traceMsg tracer "buildXrefMapFiles" $
+                  buildXrefMapFiles tracer finalCfg
+              putStrLn "Finished creating the data and saving to files."
+              traceMsg tracer "createGleanDB" $
+                createGleanDB
+                  (HieDBIndexerEnv backend finalCfg)
+                  fileLinesMap
+                  xrefMapData
