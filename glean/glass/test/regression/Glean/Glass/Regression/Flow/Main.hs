f095896fc19b5ef67b4941b8ea95d8816482ddfa
Don Stewart <don.stewart@tngtech.com>
2022-05-18 09:17:10 -0700

    Pass args through wrapper to snapshot driver (#176)
    Summary:
    Means --replace foo works in oss.
    
    Pull Request resolved: https://github.com/facebookincubator/Glean/pull/176
    
    Reviewed By: pepeiborra
    
    Differential Revision: D36411613
    
    Pulled By: simonmar
    
    fbshipit-source-id: 7fb5971c5b6c38edc8aea50f74a0db8754801969

--- glean/glass/test/regression/Glean/Glass/Regression/Flow/Main.hs
+++ glean/glass/test/regression/Glean/Glass/Regression/Flow/Main.hs
@@ -12,6 +12,6 @@ import System.Environment
 import qualified Glean.Glass.Regression.Flow as Glass
 
 main :: IO ()
-main = withArgs ["--root", path] Glass.main
+main = getArgs >>= \args -> withArgs (["--root", path] ++ args) Glass.main
   where
     path = "glean/lang/codemarkup/tests/flow/cases/xrefs"
--- glean/glass/test/regression/Glean/Glass/Regression/TypeScript/Main.hs
+++ glean/glass/test/regression/Glean/Glass/Regression/TypeScript/Main.hs
@@ -12,6 +12,6 @@ import System.Environment
 import qualified Glean.Glass.Regression.TypeScript as Glass
 
 main :: IO ()
-main = withArgs ["--root", path] Glass.main
+main = getArgs >>= \args -> withArgs (["--root", path] ++ args) Glass.main
   where
     path = "glean/lang/typescript/tests/cases/xrefs"
