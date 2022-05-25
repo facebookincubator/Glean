afaf7ebe3d997093da3d554f9f981e54428a3610
nhawkes <nhawkes@users.noreply.github.com>
2022-05-25 16:24:19 +0100

    Update Path.hs

--- glean/glass/Glean/Glass/Path.hs
+++ glean/glass/Glean/Glass/Path.hs
@@ -10,7 +10,7 @@ module Glean.Glass.Path where
 
 import qualified Data.Text as Text
 
-import Glean.Glass.Base ( SymbolRepoPath(..), GleanPath(GleanPath) )
+import Glean.Glass.Base (GleanPath (GleanPath), SymbolRepoPath (..))
 import qualified Glean.Glass.Types as Glass
 
 -- TODO: We probably want to read the repo config from a file, or put it in a
@@ -19,17 +19,17 @@ import qualified Glean.Glass.Types as Glass
 -- Convert repo-relative Glass normalized paths to Glean-index specific paths
 toGleanPath :: SymbolRepoPath -> GleanPath
 toGleanPath
-  rp@SymbolRepoPath{symbolRepo=Glass.RepoName "react"} (Glass.Path path) =
-    GleanPath ("react/" <> symbolPath rp)
-toGleanPath SymbolRepoPath{..} = GleanPath symbolPath
+  rp@SymbolRepoPath (Glass.RepoName "react") (Glass.Path path) =
+    GleanPath ("react/" <> path)
+toGleanPath SymbolRepoPath {..} = GleanPath path
 
--- | Site-level rules for processing index paths to the filesystem
--- Glass paths are always repo-root relative.
-fromGleanPath
-  :: Glass.RepoName -> GleanPath -> SymbolRepoPath
+{- | Site-level rules for processing index paths to the filesystem
+ Glass paths are always repo-root relative.
+-}
+fromGleanPath ::
+  Glass.RepoName -> GleanPath -> SymbolRepoPath
 fromGleanPath repo@(Glass.RepoName "react") (GleanPath path) =
   SymbolRepoPath repo $ case Text.stripPrefix "react/" path of
     Just suff -> Glass.Path suff
     Nothing -> Glass.Path path
-
 fromGleanPath repo (GleanPath path) = SymbolRepoPath repo $ Glass.Path path
