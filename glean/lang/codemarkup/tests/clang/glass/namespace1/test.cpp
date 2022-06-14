cf9830b06790838e2b1a1b3215e896101edb0236
Don Stewart <dons@fb.com>
2022-06-14 03:42:22 -0700

    Use a glass-specific set of test C++ files for glass regression tests
    Summary:
    We want to test slightly different behaviors than the main glean tests.
    E.g. handling of decls and defs. This starts down that path
    
    Example shows a collision in the symbol test/cpp/foo/foo, where the
    searchEntity result from Glass will be the first of either the decl or the
    definition of foo(), depending on order in the file (its ambiguous). Both
    are entities, ad codemarkup.cxx.CxxResolveDeclarationToEntity
    
    Reviewed By: simonmar
    
    Differential Revision: D37133314
    
    fbshipit-source-id: d8695ecb60b2394801df07e6bb19f070a4a1942f

---
+++ glean/lang/codemarkup/tests/clang/glass/namespace1/test.cpp
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+namespace foo {
+
+struct S {};
+
+/**
+ * Test Code Block
+ */
+void f();
+
+/* test/cpp/foo/foo */
+void foo() {
+  return;
+}
+
+/* check duplicate symbols in same namespace */
+/* test/cpp/foo/foo */
+void foo();
+
+namespace bar {
+
+struct T {};
+void g();
+
+}
+}
+
+void h() {
+  foo::S s;
+  foo::bar::T t;
+  foo::f();
+  foo::foo();
+  foo::bar::g();
+}
