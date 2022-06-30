17b5d4048688eacb23ec02cb0d5ad2532e53a3f3
Don Stewart <dons@fb.com>
2022-06-29 17:06:20 -0700

    Bulk fetch locations when doing glass.SearchRelated
    Summary:
    Get rid of extra pass of fetching symbol locations in SearchRelated results. We always need the locations, so use the combined searchRelated query that grabs them for us in one pass.
    Cleaner and more efficient. Removes a ton of duplication and the relation type and direction are pushed to Glean, so we just dispatch in Angle to difference searches.
    
    Reviewed By: nhawkes
    
    Differential Revision: D37449881
    
    fbshipit-source-id: 071792be92c1469c6815872ca2d40bb7a57095f0

--- glean/glass/Glean/Glass/SearchRelated.hs
+++ glean/glass/Glean/Glass/SearchRelated.hs
@@ -24,24 +24,20 @@ import Data.HashSet (HashSet)
 import GHC.Generics (Generic)
 import qualified Data.HashSet as HashSet
 
-import Util.Text (textShow)
-
 import qualified Glean
 import Glean.Angle as Angle
 import Glean.Haxl.Repos (RepoHaxl, ReposHaxl, withRepo)
 import qualified Glean.Schema.CodemarkupTypes.Types as Code
 import qualified Glean.Schema.Codemarkup.Types as Code
 import qualified Glean.Schema.Code.Types as Code
-import qualified Glean.Schema.Src.Types as Src
 
 import qualified Glean.Glass.Search as Search
 import Glean.Glass.Search.Class
 import Glean.Glass.Base (GleanPath (..))
-import qualified Glean.Glass.Query as Query
 import Glean.Glass.Path
 import Glean.Glass.SymbolId (entityToAngle, toSymbolId)
 import Glean.Glass.Types
-import Glean.Glass.Utils (fetchData, searchRecursiveWithLimit)
+import Glean.Glass.Utils (searchRecursiveWithLimit)
 
 -- | Whether to expand relationships recursively
 data Recursive
@@ -49,22 +45,12 @@ data Recursive
   | NotRecursive
   deriving Eq
 
--- | What kind of relationship
-data Relation
-  = Extends
-  | Contains
-  deriving Eq
-
--- | Direction of the relationship arrow to search
-data Direction
-  = Parent
-  | Child
-  deriving Eq
-
 -- | Pairs of edges of related entities
 data RelatedEntities = RelatedEntities
-  { parent :: Code.Entity
-  , child :: Code.Entity
+  { parentEntity :: Code.Entity
+  , parentLocation :: Code.Location
+  , childEntity :: Code.Entity
+  , childLocation :: Code.Location
   } deriving (Eq,Generic,Hashable)
 
 -- | Pairs of edges of related entities, with all metadata
@@ -87,47 +73,24 @@ searchRelatedEntities
   -> RepoName
   -> ReposHaxl u w [RelatedLocatedEntities]
 searchRelatedEntities limit recurse dir rel SearchEntity{..} repo =
-  withRepo entityRepo $ locateEntities repo =<< case (dir, rel) of
-    (RelationDirection_Parent, RelationType_Extends) ->
-      searchRelation limit recurse Extends Parent repo [decl] HashSet.empty
-    (RelationDirection_Child, RelationType_Extends) ->
-      searchRelation limit recurse Extends Child repo [decl] HashSet.empty
-    (RelationDirection_Parent, RelationType_Contains) ->
-      searchRelation limit recurse Contains Parent repo [decl] HashSet.empty
-    (RelationDirection_Child, RelationType_Contains) ->
-      searchRelation limit recurse Contains Child repo [decl] HashSet.empty
-    _ ->
-      pure []
+  withRepo entityRepo $ toSymbolIds repo =<<
+    searchRelation limit recurse rel dir repo [decl] HashSet.empty
 
 -- | Lift entity search results into pairs of entities that we found,
 -- along with their location and symbol id
-locateEntities
+toSymbolIds
   :: RepoName -> [RelatedEntities] -> RepoHaxl u w [RelatedLocatedEntities]
-locateEntities repo edges = mapM locatePairs edges
+toSymbolIds repo edges = mapM locatePairs edges
   where
     locatePairs RelatedEntities{..} = RelatedLocatedEntities
-      <$> mkLocate parent
-      <*> mkLocate child
-
-    mkLocate entity = do
-      loc <- locate entity
-      sym <- symbol loc
-      return (loc, sym)
-
-    symbol (entity, file, _span) = do
+      <$> mkLocate parentEntity parentLocation
+      <*> mkLocate childEntity childLocation
+    mkLocate entity Code.Location{..} = do
+      symId <- symbol entity location_file
+      return ((entity,location_file,location_location), symId)
+    symbol entity file = do
       path <- GleanPath <$> Glean.keyOf file
       toSymbolId (fromGleanPath repo path) entity
-
--- | Could batch this up with the original search
-locate :: Code.Entity -> RepoHaxl u w (ResultLocation Code.Entity)
-locate entity = case entityToAngle entity of
-  Right q -> do
-    mLoc <- fetchData (locationAngle q)
-    case mLoc of
-      Just loc -> pure loc
-      Nothing -> throwM $ ServerException $
-        "Failed to get location for: " <> textShow entity
-  Left t -> throwM (ServerException t)
 
 --
 -- Search driver, expand search until done, returning pairs of edges
@@ -136,8 +99,8 @@ locate entity = case entityToAngle entity of
 searchRelation
   :: Int
   -> Recursive
-  -> Relation
-  -> Direction
+  -> RelationType
+  -> RelationDirection
   -> RepoName
   -> [Code.Entity]
   -> HashSet RelatedEntities
@@ -148,16 +111,22 @@ searchRelation limit recursive relation direction repo toVisit visited = do
       Right angle -> return angle
       Left t -> throwM (ServerException t)
   justVisited <- case (relation, direction) of
-    (Extends, Parent) -> searchParentExtends limit angle
-    (Extends, Child) -> searchChildExtends limit angle
-    (Contains, Parent) -> return [] -- todo
-    (Contains, Child) -> searchChildContains limit angle
+    (RelationType_Extends, RelationDirection_Parent)
+      -> runSearchRelated limit angle Code.RelationType_ExtendsParentOfChild
+    (RelationType_Extends, RelationDirection_Child)
+      -> runSearchRelated limit angle Code.RelationType_ExtendsChildOfParent
+    (RelationType_Contains, RelationDirection_Parent)
+      -> pure [] -- todo
+    (RelationType_Contains, RelationDirection_Child)
+      -> runSearchRelated limit angle Code.RelationType_ContainsChildOfParent
+    _ -> pure [] -- unknown thrift case
   let
     newlyVisited = HashSet.fromList justVisited `HashSet.difference` visited
     visited' = visited `HashSet.union` newlyVisited
     toVisit = HashSet.toList $ case direction of
-      Parent -> HashSet.map parent newlyVisited
-      Child -> HashSet.map child newlyVisited
+      RelationDirection_Parent -> HashSet.map parentEntity newlyVisited
+      RelationDirection_Child -> HashSet.map childEntity newlyVisited
+      _ -> HashSet.empty -- unknown direction
     recLimit = limit - length visited'
   if
     recursive == Recursive &&
@@ -167,85 +136,48 @@ searchRelation limit recursive relation direction repo toVisit visited = do
   then
     searchRelation recLimit recursive relation direction repo toVisit visited'
   else
-   pure $ HashSet.toList visited'
+    pure $ HashSet.toList visited'
 
-searchChildContains
+runSearchRelated
   :: Int
   -> [Angle Code.Entity]
+  -> Code.RelationType
   -> RepoHaxl u w [RelatedEntities]
-searchChildContains limit angle = do
+runSearchRelated limit angle searchType = do
   entities <-
-    searchRecursiveWithLimit (Just limit) $ searchChildContainsAngle $
+    searchRecursiveWithLimit (Just limit) $
+        searchRelatedEntitiesQ searchType $
       elementsOf $ array angle
   pure $
     [ RelatedEntities
-      { parent = Code.containsChildEntity_key_parent contains
-      , child = Code.containsChildEntity_key_child contains
+      { parentEntity = parentEntity_parent
+      , childEntity = childEntity_child
+      , parentLocation = parentEntity_location
+      , childLocation = childEntity_location
       }
-    | Code.ContainsChildEntity {..} <- entities
-    , Just contains <- [containsChildEntity_key]
+    | Code.SearchRelatedEntities{..} <- entities
+    , Just Code.SearchRelatedEntities_key{..}
+       <- [searchRelatedEntities_key]
+    , let Code.ParentEntity{..} = searchRelatedEntities_key_parent
+    , let Code.ChildEntity{..} = searchRelatedEntities_key_child
     ]
 
-searchParentExtends
-  :: Int
-  -> [Angle Code.Entity]
-  -> RepoHaxl u w [RelatedEntities]
-searchParentExtends limit angle = do
-  entities <-
-    searchRecursiveWithLimit (Just limit) $ searchParentExtendsAngle $
-      elementsOf $ array angle
-  pure $
-    [ RelatedEntities
-      { parent = Code.extendsParentEntity_key_parent extends
-      , child = Code.extendsParentEntity_key_child extends
-      }
-    | Code.ExtendsParentEntity {..} <- entities
-    , Just extends <- [extendsParentEntity_key]
-    ]
-
-searchChildExtends
-  :: Int
-  -> [Angle Code.Entity]
-  -> RepoHaxl u w [RelatedEntities]
-searchChildExtends limit angle = do
-  entities <-
-    searchRecursiveWithLimit (Just limit) $ searchChildExtendsAngle $
-      elementsOf $ array angle
-  pure $
-    [ RelatedEntities
-      { parent = Code.extendsChildEntity_key_parent extends
-      , child = Code.extendsChildEntity_key_child extends
-      }
-    | Code.ExtendsChildEntity {..} <- entities
-    , Just extends <- [extendsChildEntity_key]
-    ]
-
-searchChildContainsAngle :: Angle Code.Entity -> Angle Code.ContainsChildEntity
-searchChildContainsAngle entity =
-  predicate @Code.ContainsChildEntity (
-    rec $
-      field @"parent" entity
-    end)
-
-searchParentExtendsAngle :: Angle Code.Entity -> Angle Code.ExtendsParentEntity
-searchParentExtendsAngle entity =
-  predicate @Code.ExtendsParentEntity (
-    rec $
-      field @"child" entity
-    end)
-
-searchChildExtendsAngle :: Angle Code.Entity -> Angle Code.ExtendsChildEntity
-searchChildExtendsAngle entity =
-  predicate @Code.ExtendsChildEntity (
-    rec $
-      field @"parent" entity
-    end)
-
-locationAngle :: Angle Code.Entity -> Angle (ResultLocation Code.Entity)
-locationAngle entity =
-  vars $ \(entity' :: Angle Code.Entity) (file :: Angle Src.File)
-      (rangespan :: Angle Code.RangeSpan) ->
-    tuple (entity', file, rangespan) `where_` [
-      entity' .= sig @Code.Entity entity,
-      Query.entityLocation entity file rangespan
-    ]
+--
+-- unified search by relation
+--
+searchRelatedEntitiesQ
+  :: Code.RelationType -> Angle Code.Entity -> Angle Code.SearchRelatedEntities
+searchRelatedEntitiesQ queryTy entity = predicate @Code.SearchRelatedEntities $
+  if queryTy `elem`
+     [Code.RelationType_ExtendsParentOfChild,
+      Code.RelationType_ContainsParentOfChild]
+  then
+    rec $ -- bind to child entity
+      field @"query" (enum queryTy) $
+      field @"child" (rec $ field @"child" entity end)
+    end
+  else
+    rec $ -- search by parent entities
+      field @"query" (enum queryTy) $
+      field @"parent" (rec $ field @"parent" entity end)
+    end
