261726f9d7544acdb2f1ff1d5ea756fe5aef5baf
Luke Petre <lpetre@fb.com>
2022-07-21 04:31:52 -0700

    run the python indexer for buck tests
    Summary: The buck glean index is built by the python indexer, make that more explicit in all the regression tests.
    
    Reviewed By: simonmar
    
    Differential Revision: D38010176
    
    fbshipit-source-id: cc9b51f154b8021c99a29790d64c3660a31e0bc2

--- glean/glass/test/regression/Glean/Glass/Regression/Buck.hs
+++ glean/glass/test/regression/Glean/Glass/Regression/Buck.hs
@@ -10,12 +10,20 @@ module Glean.Glass.Regression.Buck (main) where
 
 import Test.HUnit
 
-import Glean.Regression.Test
-
-import Glean.Glass.Types
+import Glean.Glass.Regression.Snapshot as Glass
 import Glean.Glass.Regression.Tests
+import Glean.Glass.Types
+import Glean.Indexer.Python as Python
+
 
 main :: IO ()
-main = mainTestIndexExternal "glass-regression-buck" $ \get -> TestList
+main = mainGlassSnapshot testName testPath testIndexer unitTests
+  where
+    testName = "glass-regression-buck"
+    testPath = "glean/glass/test/regression/tests/buck"
+    testIndexer = Python.indexer
+
+unitTests :: Getter -> [Test]
+unitTests get =
   [ testDocumentSymbolListX (Path "buck_project/TEST_TARGETS") get
   ]
