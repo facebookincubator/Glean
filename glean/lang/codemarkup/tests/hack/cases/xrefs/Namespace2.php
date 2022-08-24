e08b99b9ba459c0cab4fe589bdbe414f42a6c500
Don Stewart <dons@fb.com>
2022-08-24 03:51:44 -0700

    Add tests for namespace symbols
    Summary:
    Namespaces can be split over several files, resulting in entities with mulitple locations.
    Let's have a test for this.
    
    - adds Namespace2.php , with a second decl of the NS namespace
    - a describe query which picks up the other_location
    
    Reviewed By: lazamar
    
    Differential Revision: D38966130
    
    fbshipit-source-id: 09a859aa6df531fb2f6861fdf8bff8e2c758fb43

---
+++ glean/lang/codemarkup/tests/hack/cases/xrefs/Namespace2.php
@@ -0,0 +1,16 @@
+<?hh
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// example of namespace in two files
+namespace NS;
+
+class AnotherNamespaceClass {
+    public function another_class_function(): void {
+    }
+}
