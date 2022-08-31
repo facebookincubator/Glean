b773d4e031f9e258f7614e78bb4e95a61a76b3de
Don Stewart <don.stewart@tngtech.com>
2022-08-31 15:42:58 +1000

    Fix test with more shims

--- glean/github/ServiceData/GlobalStats.hs
+++ glean/github/ServiceData/GlobalStats.hs
@@ -9,6 +9,7 @@
 module ServiceData.GlobalStats (module ServiceData.GlobalStats) where
 
 import Data.ByteString (ByteString)
+import qualified Data.HashMap.Strict as HashMap
 
 import ServiceData.Types
 
@@ -17,6 +18,9 @@ setCounter _ _ = return ()
 
 clearCounter :: ByteString -> IO ()
 clearCounter _ = return ()
+
+getCounters :: IO (HashMap.HashMap ByteString Int)
+getCounters = return HashMap.empty
 
 addStatValue :: ByteString -> Int -> IO ()
 addStatValue _ _ = return ()
