117bc1c57c5789ddcdf88fcc4df9ca9129e6ee7e
Roman Leshchinskiy <rleshchinskiy@gmail.com>
2022-09-03 21:24:15 +0200

    Don't include key when serialising fact iterators
    We previously started serialising fact ids in addition to keys for
    iterators in query continuations. This diff now stops serialising keys
    which should make the continuations significantly smaller.

--- glean/if/internal.thrift
+++ glean/if/internal.thrift
@@ -14,9 +14,8 @@ namespace hs Glean
 
 struct KeyIterator {
   // id of fact the iterator is pointing to
-  7: optional glean.Id fact;
+  7: glean.Id fact;
   1: i64 type;
-  2: optional string key;
   // size of prefix - the value of the prefix can be obtained from the current
   // fact's key
   3: i64 prefix_size;
