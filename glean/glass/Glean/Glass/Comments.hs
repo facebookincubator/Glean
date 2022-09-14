d40f786a2e3f67087f6b602b26af7762a999bd6f
Don Stewart <dons@fb.com>
2022-09-14 12:07:52 -0700

    Port glass to new codemarkup.EntityDocumentation
    Summary: Same types, just disentangled from docmarkup workflow
    
    Reviewed By: nhawkes
    
    Differential Revision: D39494039
    
    fbshipit-source-id: 182de9330b2c9fcbc0068985b3e93c4fea5fcc80

--- glean/glass/Glean/Glass/Comments.hs
+++ glean/glass/Glean/Glass/Comments.hs
@@ -9,39 +9,60 @@
 {-# LANGUAGE ApplicativeDo #-}
 {-# LANGUAGE TypeApplications #-}
 
-module Glean.Glass.Comments
-  ( getCommentsForEntity
+module Glean.Glass.Comments (
+    getCommentsForEntity
   ) where
 
 import Data.Text (Text)
 
-import qualified Glean
 import Glean.Angle as Angle
 import Glean.Glass.Range ( rangeSpanToLocationRange )
 import qualified Glean.Haxl.Repos as Glean
-import qualified Glean.Schema.Docmarkup.Types as Docmarkup
 import qualified Glean.Schema.Code.Types as Code
+import qualified Glean.Schema.Src.Types as Src
 import qualified Glean.Schema.CodemarkupTypes.Types as Code
+import qualified Glean.Schema.Codemarkup.Types as Code
 
 import Glean.Glass.SymbolId (entityToAngle)
 import Glean.Glass.Utils ( searchRecursiveWithLimit )
 import qualified Glean.Glass.Types as Glass
 
+-- | Set a low bound on maximum results so we don't search too long
+mAX_COMMENTS :: Int
+mAX_COMMENTS = 3
+
 getCommentsForEntity
   :: Glass.RepoName
   -> Code.Entity
   -> Glean.RepoHaxl u w (Either Text [Glass.LocationRange])
-getCommentsForEntity repo entity = mapM anns $ entityToAngle entity
-  where
-    anns entityAngle = do
-      preds <- searchRecursiveWithLimit Nothing $
-          Angle.predicate @Docmarkup.EntityComments (
-            rec $
-              field @"entity" entityAngle
-            end)
-      keys <- mapM Glean.keyOf preds
-      mapM getLocationRange keys
+getCommentsForEntity repo entity = case entityToAngle entity of
+  Left err -> pure (Left err)
+  Right q -> Right <$> getEntityDocumentation repo q
 
-    getLocationRange Docmarkup.EntityComments_key{..} =
-      rangeSpanToLocationRange repo entityComments_key_file $
-        Code.RangeSpan_span entityComments_key_span
+getEntityDocumentation
+  :: Glass.RepoName
+  -> Angle Code.Entity
+  -> Glean.RepoHaxl u w [Glass.LocationRange]
+getEntityDocumentation repo entity = do
+  res <- searchRecursiveWithLimit (Just mAX_COMMENTS) $ getDocumentation entity
+  mapM (uncurry (mkLocationRange repo)) res
+
+-- | Docs are always bytespans at the moment. Convert them out.
+mkLocationRange
+   :: Glass.RepoName -> Src.File -> Src.ByteSpan
+   -> Glean.RepoHaxl u w Glass.LocationRange
+mkLocationRange repo file span = rangeSpanToLocationRange repo file
+  (Code.RangeSpan_span span)
+
+getDocumentation :: Angle Code.Entity -> Angle (Src.File, Src.ByteSpan)
+getDocumentation entity =
+  vars $ \ (file :: Angle Src.File) (span :: Angle Src.ByteSpan) ->
+  tuple (file, span) `where_` [
+    wild .= Angle.predicate @Code.EntityDocumentation (
+      rec $
+        field @"entity" entity $
+        field @"file" (asPredicate file) $
+        field @"span" span
+      end
+    )
+  ]
