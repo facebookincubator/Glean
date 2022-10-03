e2d1a999188097c2e364c4b6f19244d371c0c812
Simon Marlow <smarlow@meta.com>
2022-10-03 04:18:00 -0700

    Remove Thrift queries from the benchmark
    Summary: Killing Thrift queries as far as possible
    
    Reviewed By: pepeiborra
    
    Differential Revision: D39972878
    
    fbshipit-source-id: 59367cdf7a7931d0b4859dc34b795991c27b29ca

--- glean/bench/QueryBench.hs
+++ glean/bench/QueryBench.hs
@@ -14,19 +14,16 @@ import Control.Concurrent.Async
 import Control.Monad (void)
 import Criterion.Types
 import Data.Default
-import Data.Text (Text)
 
 import Util.String.Quasi
 
 import Glean
-import Glean.Query.Thrift.Internal
+import Glean.Angle as Angle
 import qualified Glean.Schema.CodeCxx.Types as Code.Cxx
-import qualified Glean.Schema.Cxx1.Types as Cxx
-import qualified Glean.Schema.Sys.Types as Sys
-import qualified Glean.Schema.Query.Sys.Types as Query.Sys
-import qualified Glean.Schema.Query.Cxx1.Types as Query.Cxx
-import qualified Glean.Schema.Query.GleanTest.Types as Query.Glean.Test
 import qualified Glean.Schema.Codemarkup.Types as Codemarkup
+import qualified Glean.Schema.Cxx1.Types as Cxx
+import qualified Glean.Schema.GleanTest.Types as Glean.Test
+import qualified Glean.Schema.Sys.Types as Sys
 import Glean.Util.Benchmark
 
 import BenchDB
@@ -34,34 +31,21 @@ import BenchDB
 main :: IO ()
 main = benchmarkMain $ \run -> withBenchDB 10000 $ \env repo -> do
   let
-    simpleQueryThrift = query $ Query.Sys.Blob_with_key "x1"
-    nestedQueryThrift = query $ Query.Cxx.FunctionName_with_key $ def
-      { Query.Cxx.functionName_key_name = Just $
-          Query.Cxx.Name_with_key "x1" }
-    complexQueryThrift = query $ Query.Glean.Test.Predicate_with_key $ def
-      { Query.Glean.Test.kitchenSink_string_ = Just "x1" }
-
     nestedAngle :: Query Cxx.FunctionName
     nestedAngle = angle "cxx1.FunctionName { name = \"x1\" }"
 
-    pageQueryThrift :: Glean.Query Cxx.Name
-    pageQueryThrift = limit 1 $ mkQuery ("{\"key\":{\"prefix\":\"y\"}}" :: Text)
-
     pageAngle :: Query Cxx.Name
     pageAngle = limit 1 $ angle "cxx1.Name \"y\".."
-
-    -- this is the slow case for the JSON query API - when a nested
-    -- query matches multiple facts (in this case 1000), the
-    -- re-serialized query contains all 1000 matches.
-    pageNestedQueryThrift :: Glean.Query Cxx.FunctionName
-    pageNestedQueryThrift = limit 1 $ mkQuery
-      ("{\"key\":{\"name\":{\"key\":{\"prefix\":\"y\"}}}}" :: Text)
 
     pageNestedAngle :: Query Cxx.FunctionName
     pageNestedAngle = limit 1 $ angle "cxx1.FunctionName { name = \"y\".. }"
 
     simpleAngle :: Query Sys.Blob
     simpleAngle = angle "sys.Blob \"x1\""
+
+    complexQueryAngle :: Query Glean.Test.Predicate
+    complexQueryAngle = Angle.query $ predicate @Glean.Test.Predicate $ rec $
+      field @"string_" (string "x1") end
 
     -- test compiling a complex derived predicate, should let us
     -- spot regressions in the compiler pipeline.
@@ -101,24 +85,11 @@ main = benchmarkMain $ \run -> withBenchDB 10000 $ \env repo -> do
     , kickOff_dependencies = Just $ Dependencies_stacked repo
     }
   run
-    [ bgroup "thrift"
-      [ bench "simple" $ whnfIO $
-          runQuery env repo simpleQueryThrift
-      , bench "nested" $ whnfIO $
-          runQuery env repo nestedQueryThrift
-      , bench "complex" $ whnfIO $
-          runQuery env repo complexQueryThrift
-      , bench "complex-parallel" $ whnfIO
+      [ bench "complex-parallel" $ whnfIO
         $ mapConcurrently (\io -> io >>= \x -> x `seq` return x)
         $ replicate 1000
-        $ runQuery env repo complexQueryThrift
-      , bench "page" $ whnfIO $
-          runQuery_ env repo pageQueryThrift
-      , bench "pagenested" $ whnfIO $
-          runQuery_ env repo pageNestedQueryThrift
-      ]
-    , bgroup "angle"
-      [ bench "simple" $ whnfIO $
+        $ runQuery env repo complexQueryAngle
+      , bench "simple" $ whnfIO $
           runQuery env repo simpleAngle
       , bench "nested" $ whnfIO $
           runQuery env repo nestedAngle
@@ -141,4 +112,3 @@ main = benchmarkMain $ \run -> withBenchDB 10000 $ \env repo -> do
             runQuery_ env repo pageNestedAngle
         ]
       ]
-    ]
