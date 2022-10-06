fb89fafb01d97ed0bedcfa6e4dfac6b4ebe55378
Don Stewart <dons@meta.com>
2022-10-06 16:25:30 -0700

    Remove another dead predicate
    Summary:
    fixed xrefs used to be keyed by file and FileXRef fact. This was redundant and Glass doesn't do this now.
    Just use CxxFileEntityXMapFixedXRefLocations which takes the FileXRef fact directly.
    
    Reviewed By: pepeiborra
    
    Differential Revision: D40131726
    
    fbshipit-source-id: c547c7e6250ac82e016a150dbfd9d3a74cc9abbe

--- glean/schema/py/codemarkupcxx.py
+++ glean/schema/py/codemarkupcxx.py
@@ -37,7 +37,6 @@ from glean.schema.codemarkup_cxx.types import (
     CxxResolveDeclarationToEntity,
     CxxContainsParentEntity,
     CxxXRefTargetLocation,
-    CxxFileEntityTraceFixedXRefLocations,
     CxxEntityInfo,
     CxxDefinitionContainsParent,
     CxxNamespaceDeclarationContainsChild,
@@ -335,18 +334,6 @@ class CodemarkupCxxCxxXRefTargetLocation(GleanSchemaPredicate):
 
   @staticmethod
   def angle_query(*, target: Optional["Cxx1XRefTarget"] = None, entity: Optional["CodeCxxEntity"] = None, location: Optional["CodemarkupTypesLocation"] = None) -> "CodemarkupCxxCxxXRefTargetLocation":
-    raise Exception("this function can only be called from @angle_query")
-
-
-
-class CodemarkupCxxCxxFileEntityTraceFixedXRefLocations(GleanSchemaPredicate):
-  @staticmethod
-  def build_angle(__env: Dict[str, R], file: ast.Expr, trace: ast.Expr, xref: ast.Expr, entity: ast.Expr) -> Tuple[str, Struct]:
-    query_fields =  ', '.join(filter(lambda x: x != '', [angle_for(__env, file, 'file'), angle_for(__env, trace, 'trace'), angle_for(__env, xref, 'xref'), angle_for(__env, entity, 'entity')]))
-    return f"codemarkup.cxx.CxxFileEntityTraceFixedXRefLocations.4 { ('{ ' + query_fields + ' }') if query_fields else '_' }", CxxFileEntityTraceFixedXRefLocations
-
-  @staticmethod
-  def angle_query(*, file: Optional["SrcFile"] = None, trace: Optional["Cxx1FileXRefMap"] = None, xref: Optional["CodemarkupTypesXRefLocation"] = None, entity: Optional["CodeCxxEntity"] = None) -> "CodemarkupCxxCxxFileEntityTraceFixedXRefLocations":
     raise Exception("this function can only be called from @angle_query")
 
 
