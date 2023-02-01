df90483a437661d7756069f9155b84eade284453
Don Stewart <dons@meta.com>
2023-01-31 17:37:32 -0800

    Simplify a bit of Haskell
    Summary: Just refactoring
    
    Reviewed By: nhawkes
    
    Differential Revision: D42877656
    
    fbshipit-source-id: 7f9adfddc0310df55c5f0a30d0c32f12ba9cb9d5

--- glean/glass/Glean/Glass/Pretty/Python.hs
+++ glean/glass/Glean/Glass/Pretty/Python.hs
@@ -13,7 +13,8 @@ module Glean.Glass.Pretty.Python
     prettyPythonSignature
   ) where
 
-import Data.Text ( Text, takeWhileEnd, intercalate )
+import Data.Text ( Text, takeWhileEnd )
+import qualified Data.Text as Text
 import Data.Text.Prettyprint.Doc
 
 import qualified Glean
@@ -29,18 +30,17 @@ prettyPythonSignature
   :: LayoutOptions
   -> Python.Entity
   -> Glean.RepoHaxl u w (Maybe (SimpleDocStream (Maybe SymbolId)))
-prettyPythonSignature opts entity = case entity of
-    Python.Entity_decl decl  -> do
-      mDef <- fetchDataRecursive (anglePythonDeclDef (toAngle decl))
-      case mDef of
-        Nothing -> return Nothing
-        Just def -> ppr =<< prettyDef def
-    Python.Entity_EMPTY -> return Nothing
+prettyPythonSignature opts (Python.Entity_decl decl) = do
+  mDef <- fetchDataRecursive (anglePythonDeclDef (toAngle decl))
+  case mDef of
+    Nothing -> return Nothing
+    Just def -> ppr =<< prettyDef def
   where
     ppr mText = do
       let docStream = layoutSmart opts . pretty <$> mText
       return $ reAnnotateS (const Nothing) <$> docStream
 
+prettyPythonSignature _ Python.Entity_EMPTY = return Nothing
 
 anglePythonDeclDef :: Angle Python.Declaration -> Angle Python.Definition
 anglePythonDeclDef decl = var $ \(def :: Angle Python.Definition) ->
@@ -54,28 +54,28 @@ anglePythonDeclDef decl = var $ \(def :: Angle Python.Definition) ->
   ]
 
 prettyDef :: Python.Definition -> Glean.RepoHaxl u w (Maybe Text)
-prettyDef def = do
-  case def of
-    Python.Definition_func x -> do
-      Python.FunctionDefinition_key {
-        functionDefinition_key_declaration = decl,
-        functionDefinition_key_is_async = async,
-        functionDefinition_key_decorators = decors
-      }  <- Glean.keyOf x
-      Python.FunctionDeclaration_key name  <- Glean.keyOf decl
-      Just <$>
-        pure (fmtDecorators decors) <>
-        (if async then "async " else "") <>
-        "def " <> (trimModule <$> Glean.keyOf name) <>
-        "(...) -> ...:"
-    Python.Definition_EMPTY -> return Nothing
-    _ -> return Nothing
+prettyDef (Python.Definition_func def) = do
+  Python.FunctionDefinition_key {
+    functionDefinition_key_declaration = decl,
+    functionDefinition_key_is_async = async,
+    functionDefinition_key_decorators = decors }  <- Glean.keyOf def
+  Python.FunctionDeclaration_key name  <- Glean.keyOf decl
+  Just <$>
+    pure (fmtDecorators decors) <>
+    (if async then "async " else "") <>
+    "def " <> (trimModule <$> Glean.keyOf name) <>
+    "(...) -> ...:"
 
+prettyDef Python.Definition_cls{} = return Nothing
+prettyDef Python.Definition_variable{} = return Nothing
+prettyDef Python.Definition_module{} = return Nothing
+prettyDef Python.Definition_EMPTY = return Nothing
+
+-- | we could use the sname here to lookup the associated decl fact
+-- as an xref in the type signature (c.f how Hack does this)
 trimModule :: Text -> Text
 trimModule qname = takeWhileEnd (/='.') qname
 
 fmtDecorators :: Maybe [Python.Decorator] -> Text
-fmtDecorators mDecors =
-  case mDecors of
-    Nothing -> ""
-    Just decors -> intercalate "\n" decors <> "\n"
+fmtDecorators Nothing = ""
+fmtDecorators (Just decors) = Text.unlines decors
