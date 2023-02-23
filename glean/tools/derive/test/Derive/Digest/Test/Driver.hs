b5a608327e1b978bca743359c35695a46788d015
Pepe Iborra <pepeiborra@meta.com>
2023-02-23 03:31:28 -0800

    benchmarking regression test
    Summary:
    Benchmarking target to conveniently measure the cost of running the digest standalone deriver.
    
    The implementation reuses the glean regression test framework to setup a Hack DB containing ~10000 declarations and then run the deriver on it, printing time and allocations to stdout.
    
    I opted for Hack instead of Python because the Python indexer was much slower to index a file with 2000 declarations.
    
    Reviewed By: phlalx
    
    Differential Revision: D43446270
    
    fbshipit-source-id: 854a212d8872f76ccc94ab82c3586a7aa99cd993

--- glean/tools/derive/test/Derive/Digest/Test/Driver.hs
+++ glean/tools/derive/test/Derive/Digest/Test/Driver.hs
@@ -6,32 +6,66 @@
   LICENSE file in the root directory of this source tree.
 -}
 
+{-# LANGUAGE ApplicativeDo #-}
 module Derive.Digest.Test.Driver
   ( main
   ) where
 
-import System.FilePath ((</>))
+import Control.Monad (void)
+import System.FilePath ((</>), splitFileName, joinPath, splitPath)
+
+import Util.Timing
 
 import Glean.Indexer
 import Glean.Indexer.List (cmdLineParser)
 import Glean.Regression.Snapshot.Driver
 import Glean.Regression.Snapshot
-import Glean.Derive.Digest.Lib
+import Glean.Derive.Digest.Lib ( derive, Config(..) )
+import Data.Hashable (hash)
+import Options.Applicative
+import TextShow (showt)
 
-driver :: Driver RunIndexer
+data Options = Options
+  { runIndexerOptions :: RunIndexer
+  , deriveConfig :: Config
+  }
+
+optionsParser :: Parser Options
+optionsParser = do
+  runIndexerOptions <- cmdLineParser
+  hashDigests <- switch (long "hash-digests")
+  stripDepth <- option auto (long "strip" <> help "strip depth" <> value 0)
+  return Options{
+    runIndexerOptions = runIndexerOptions,
+    deriveConfig = Config{
+      hashFunction = if hashDigests then showt . hash else id,
+      pathAdaptor = stripPath stripDepth
+      }
+  }
+
+-- | stripPath 1 "www/take/me/home.php"  = "take/me/home.php"
+stripPath :: Int -> FilePath -> FilePath
+stripPath 0 path = path
+stripPath depth path = stripped_body </> file
+  where
+    (body,file) = splitFileName path
+    stripped_body = joinPath $ drop depth $ splitPath body
+
+driver :: Driver Options
 driver = driverFromIndexer $
   Indexer {
     indexerShortName = "multiple",
     indexerDescription = "Choose an indexer to run",
-    indexerOptParser = cmdLineParser,
-    indexerRun = id
-    } `indexerThen` deriveDigests
+    indexerOptParser = optionsParser ,
+    indexerRun = runIndexerOptions <> deriveDigests
+    }
   where
-    deriveDigests backend repo params =
-      derive backend repo Config
-        { hashFunction = id
-        , pathAdaptor = (indexerRoot params </>)
-        }
+    deriveDigests opts backend repo params =
+      void $ reportAndShowTime "derive digests" $ derive backend repo
+        (deriveConfig opts)
+          { pathAdaptor =
+            (indexerRoot params </>) . pathAdaptor (deriveConfig opts)
+          }
 
 main :: IO ()
 main = testMain driver
