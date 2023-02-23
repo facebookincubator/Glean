96a9374ab04f52f08a9d95b1d822d68d3b45116f
Marcelo Lazaroni <lazamar@meta.com>
2023-02-23 05:05:25 -0800

    Add parallel reads
    Summary:
    We were bottlenecked on the reading speed,
    Here we allow multiple threads to read in parallel, all of them dumping data into the parallel dedupe queue.
    
    Reviewed By: simonmar
    
    Differential Revision: D41549768
    
    fbshipit-source-id: 4043a2cd529665e7312f3678237f8208b0f5792d

--- glean/tools/diff/lib.cpp
+++ glean/tools/diff/lib.cpp
@@ -29,6 +29,7 @@
 #include <future>
 #include <queue>
 #include <list>
+#include <atomic>
 
 using namespace facebook::hs;
 using namespace facebook::glean;
@@ -144,6 +145,30 @@ private:
 }
 
 extern "C" {
+
+// To ensure a fact is not in the database we must query for it after having
+// considered all facts that came before it. This is naturally sequential. To
+// decrease the bottleneck of the final dedupe we try to dedupe as much as
+// possible in parallel before the final deduplication.
+//
+// We don't want very large deduping batches or too many parallel deduplication
+// threads as each fact in one of those threads may not be adequately renamed
+// if it makes references to facts that are still in the other threads, taking
+// it to the final deduplication step.
+//
+// With efficient deduplication we get bottlenecked on the read side, so we
+// also add parallel reading threads.
+//
+//
+//  read ┐                    ┌ dedupe ┐
+//       │                    │        │
+//       │  queue_undeduped   │        │  queue_deduped
+//  read ┿──────────────────> ┿ dedupe ┿────────────────> final dedupe
+//       │                    │        │
+//       │                    │        │
+//  read ┘                    └ dedupe ┘
+//
+//
 const char *glean_diff(
     Inventory *second_inventory,
     Lookup *first_lookup,
@@ -163,17 +188,17 @@ const char *glean_diff(
   const auto first_size = distance(first_starting, first_boundary);
   TSubstitution subst(second_starting, second_size);
 
-  const auto parallel_dedupes = 25;
+  const auto parallel_dedupes = 10;
+  const auto parallel_reads = 10;
 
   // The queue_undeduped queue can be as large as we want, but must not be too small
   // otherwise dedupers might lay idle.
-  Queue queue_undeduped(20 * parallel_dedupes);
+  Queue queue_undeduped(2 * parallel_dedupes);
 
   // We want the writer to always have things to deduplicate but the larger this queue the
   // less useful are the susbstituions that we have. So we want to keep it as low as possible
   // without ever allowing the writer to be blocked.
-  Queue queue_deduped(20 * parallel_dedupes);
-
+  Queue queue_deduped(2 * parallel_dedupes);
 
   enum class SearchResult { Found, NotFound, Invalid };
 
@@ -271,25 +296,26 @@ const char *glean_diff(
     return folly::unit;
   });
 
-  size_t one_percent = second_size / (100 * batch_size);
-  size_t announce = one_percent; // announce on every one percent of batches
-
-  auto cmp = [](const Batch& one, const Batch& two) {
-    return one.sequence_number > two.sequence_number;
-  };
-  // min-heap
-  std::priority_queue<Batch, std::vector<Batch>, decltype(cmp)> queued {};
-  size_t next_index = 0;
   // start final single-threaded deduplication
   folly::Future<folly::Unit> final_dedupe = folly::via(executor, [&](){
+    size_t one_percent = std::max((size_t) 1, second_size / (100 * batch_size));
+    size_t announce = one_percent; // announce on every one percent of batches
+    auto cmp = [](const Batch& one, const Batch& two) {
+      return one.sequence_number > two.sequence_number;
+    };
+    // min_heap
+    std::priority_queue<Batch, std::vector<Batch>, decltype(cmp)> queued {};
+
+    size_t next_index = 0;
+    size_t max_queue_size = 0;
     consume(queue_deduped, [&](Batch batch) {
-      queued.push(batch);
+      queued.push(std::move(batch));
+      max_queue_size = std::max(max_queue_size, queued.size());
       while (!queued.empty() && queued.top().sequence_number == next_index) {
         auto next = queued.top();
-        auto remaining = dedupe(next);
+        dedupe(next);
         queued.pop();
         next_index++;
-
         if (--announce == 0) {
           announce = one_percent;
           auto percentage = next.sequence_number / one_percent;
@@ -300,24 +326,62 @@ const char *glean_diff(
     return folly::unit;
   });
 
-  folly::Future<folly::Unit> read_db = folly::via(executor, [&](){
-    auto i = second_lookup->enumerate();
-    auto ref = i->get();
-    size_t batch_ix = 0;
-    while (ref.id != Id::invalid()) {
-      Batch batch { batch_ix, std::vector<std::shared_ptr<Fact>>() };
-      batch.facts.reserve(batch_size);
+  const size_t batches_per_group = 10;
+  using Group = std::vector<Batch>;
+  auto read_batch_group = [&](const size_t batch_index){
+    Group group {};
 
-      // fill one batch
+    const Id start = second_lookup->startingId() + batch_index * batches_per_group * batch_size ;
+    if (start >= second_lookup->firstFreeId()) {
+      return group;
+    }
+
+    const Id end = start + batch_size * batches_per_group;
+    auto i = second_lookup->enumerate(start, end);
+    auto ref = i->get();
+    while (ref && group.size() < batches_per_group) {
+      const auto sequence_number = batch_index * batches_per_group + group.size();
+      Batch batch { sequence_number, {} };
+      batch.facts.reserve(batch_size);
       while (ref && batch.facts.size() < batch_size) {
         batch.facts.push_back(Fact::create(ref));
         i->next();
         ref = i->get();
       }
-
-      write(queue_undeduped, batch);
-      batch_ix++;
+      group.push_back(batch);
     }
+    return group;
+  };
+
+  // we start parallel_reads threads initially and start another read whenever
+  // the oldest one of these threads finishes pushing all of its batches.
+  folly::Future<folly::Unit> read_db = folly::via(executor, [&](){
+    std::atomic<int> next_batch = 0;
+    std::list<folly::Future<Group>> read_queue {};
+    bool finished_reading = false;
+
+    for (auto i = 0; i < parallel_reads; i++) {
+      read_queue.push_back(folly::via(executor, [&](){
+        auto next = next_batch++;
+        return read_batch_group(next);
+      }));
+    }
+
+    while (read_queue.size()) {
+      Group group = read_queue.front().via(executor).get();
+      read_queue.pop_front();
+      bool finished_reading = group.size() < batches_per_group;
+      if (!finished_reading) {
+        read_queue.push_back(folly::via(executor, [&](){
+          auto next = next_batch++;
+          return read_batch_group(next);
+        }));
+      }
+      for (auto& batch : group) {
+        write(queue_undeduped, batch);
+      }
+    }
+
     terminate_consumers(queue_undeduped);
     return folly::unit;
   });
