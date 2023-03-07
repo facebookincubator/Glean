0c06351f17678bff3adb55bf922c5daebfd5cf00
Don Stewart <dons@meta.com>
2023-03-06 19:57:19 -0800

    Split Python type sig generation code into Angle processing and ppr phases
    Summary:
    Similar to the Hack design, we'll take the Glean data (with possibly extra calls) into a ADT that specifies the type sig. Then we just ppr it with spliced xrefs.
    
    Step 1 : split out the query and generation steps and start writing down the ADT.
    
    Reviewed By: eguitarz
    
    Differential Revision: D43726558
    
    fbshipit-source-id: 17ff774920b07935437235f5ef30828fc08eb953

--- glean/glass/Glean/Glass/Pretty/Python.hs
+++ glean/glass/Glean/Glass/Pretty/Python.hs
@@ -23,26 +23,132 @@ import Glean.Schema.CodePython.Types as Python ( Entity(..) )
 import qualified Glean.Schema.Python.Types as Python
 import Glean.Util.ToAngle ( ToAngle(toAngle) )
 import Glean.Glass.Types ( SymbolId )
-import Glean.Glass.Utils (fetchDataRecursive )
+import Glean.Glass.Utils ( fetchDataRecursive )
+
+-- Type of Python entity signatures to capture the the subset we generate
+-- We use this to separate the processing of the Glean type from the text
+-- we wish to generate
+
+-- Python symbol kinds
+data Definition
+  = Function !AsyncModifier !Name [Parameter] !(Maybe PyType)
+ -- Class
+ -- Variable
+ -- Import
+ -- Module
+
+data Parameter = Parameter !Name (Maybe ExprText) !(Maybe PyType)
+
+-- modifiers
+data AsyncModifier = Async | NotAsync deriving (Eq)
+
+newtype ExprText = ExprText Text
+
+-- names
+newtype Name = Name Text
+
+newtype PyType = PyType Text
 
 prettyPythonSignature
   :: LayoutOptions
   -> Python.Entity
   -> Glean.RepoHaxl u w (Maybe (SimpleDocStream (Maybe SymbolId)))
 prettyPythonSignature opts (Python.Entity_decl decl) = do
-  mDef <- fetchDataRecursive (anglePythonDeclDef (toAngle decl))
-  case mDef of
+  mQuery <- fetchDataRecursive (angleDeclToDef (toAngle decl))
+  case mQuery of
     Nothing -> return Nothing
-    Just def -> ppr =<< prettyDef def
+    Just pyDef -> do
+      mDef <- fromAngleDefinition pyDef
+      return $ case mDef of
+        Nothing -> Nothing
+        Just def -> Just (doLayout (pprDefinition def))
   where
-    ppr mText = do
-      let docStream = layoutSmart opts . pretty <$> mText
-      return $ reAnnotateS (const Nothing) <$> docStream
+    doLayout doc = reAnnotateS (const Nothing) (layoutSmart opts doc)
 
 prettyPythonSignature _ Python.Entity_EMPTY = return Nothing
 
-anglePythonDeclDef :: Angle Python.Declaration -> Angle Python.Definition
-anglePythonDeclDef decl = var $ \(def :: Angle Python.Definition) ->
+fromAngleDefinition
+  :: Python.Definition -> Glean.RepoHaxl u w (Maybe Definition)
+fromAngleDefinition def = case def of
+  Python.Definition_func fn -> Just <$>
+    (fromFunctionDefinition =<< Glean.keyOf fn)
+  _ -> pure Nothing
+
+fromFunctionDefinition
+  :: Python.FunctionDefinition_key -> Glean.RepoHaxl u w Definition
+fromFunctionDefinition def = do
+  Python.FunctionDeclaration_key name <- Glean.keyOf decl
+  returnTy <- case mReturnTy of
+    Nothing -> pure Nothing
+    Just tyInfo -> Just <$> fromTypeInfo tyInfo
+  nameStr <- Glean.keyOf name
+  params <- mapM fromParameter pyParams
+  return $ Function
+    (if async then Async else NotAsync)
+    (trimModule nameStr)
+    params
+    returnTy
+  where
+    Python.FunctionDefinition_key {
+      functionDefinition_key_declaration = decl,
+      functionDefinition_key_is_async = async,
+      functionDefinition_key_params = pyParams,
+      functionDefinition_key_returnsInfo = mReturnTy
+    } = def
+
+fromTypeInfo  :: Python.TypeInfo -> Glean.RepoHaxl u w PyType
+fromTypeInfo Python.TypeInfo{..} = PyType <$> Glean.keyOf
+  typeInfo_displayType
+
+fromParameter :: Python.Parameter -> Glean.RepoHaxl u w Parameter
+fromParameter Python.Parameter{..} = do
+  nameStr <- Glean.keyOf parameter_name
+  tyInfo <- case parameter_typeInfo of
+    Nothing -> return Nothing
+    Just ty -> Just <$> fromTypeInfo ty
+  return $ Parameter (Name nameStr) (ExprText <$> parameter_value) tyInfo
+
+pprDefinition :: Definition -> Doc ()
+-- empty param case
+pprDefinition (Function async name [] returnTy) =
+  hcat [
+   pprAsync async, "def" <+> pprName name, "()",
+   case returnTy of
+      Nothing -> emptyDoc
+      Just ty -> space <> "->" <+> pprType ty
+  ]
+-- full param list
+pprDefinition (Function async name params returnTy) =
+  vcat [
+    nest 4 (vsep (
+      hcat [pprAsync async, "def" <> space, pprName name <> lparen] :
+      punctuate comma (map pprParam params)
+    )),
+    rparen <+> case returnTy of
+      Nothing -> emptyDoc
+      Just ty -> "->" <+> pprType ty
+    ]
+
+pprAsync :: AsyncModifier -> Doc ()
+pprAsync Async = "async" <> space
+pprAsync _ = emptyDoc
+
+pprParam :: Parameter -> Doc ()
+pprParam (Parameter name _mDefValue mty) = hcat
+  [ pprName name
+  , case mty of
+    Nothing -> emptyDoc
+    Just ty -> colon <+> pprType ty
+  ]
+
+pprName :: Name -> Doc ()
+pprName (Name name) = pretty name
+
+pprType :: PyType -> Doc ()
+pprType (PyType ty) = pretty ty
+
+angleDeclToDef:: Angle Python.Declaration -> Angle Python.Definition
+angleDeclToDef decl = var $ \(def :: Angle Python.Definition) ->
   def `where_` [
     wild .= predicate @Python.DeclarationDefinition (
       rec $
@@ -52,24 +158,7 @@ anglePythonDeclDef decl = var $ \(def :: Angle Python.Definition) ->
     )
   ]
 
-prettyDef :: Python.Definition -> Glean.RepoHaxl u w (Maybe Text)
-prettyDef (Python.Definition_func def) = do
-  Python.FunctionDefinition_key {
-    functionDefinition_key_declaration = decl,
-    functionDefinition_key_is_async = async
-   }  <- Glean.keyOf def
-  Python.FunctionDeclaration_key name  <- Glean.keyOf decl
-  Just <$>
-    (if async then "async " else "") <>
-    "def " <> (trimModule <$> Glean.keyOf name) <>
-    "(...) -> ...:"
-
-prettyDef Python.Definition_cls{} = return Nothing
-prettyDef Python.Definition_variable{} = return Nothing
-prettyDef Python.Definition_module{} = return Nothing
-prettyDef Python.Definition_EMPTY = return Nothing
-
 -- | we could use the sname here to lookup the associated decl fact
 -- as an xref in the type signature (c.f how Hack does this)
-trimModule :: Text -> Text
-trimModule qname = takeWhileEnd (/= '.') qname
+trimModule :: Text -> Name
+trimModule qname = Name (takeWhileEnd (/= '.') qname)
