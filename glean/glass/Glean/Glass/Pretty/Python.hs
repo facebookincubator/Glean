98baf835d793009e2517176828adcaf064878482
Don Stewart <dons@meta.com>
2023-03-08 12:10:50 -0800

    Plumb type fragment xrefs through the signature pretty printer
    Summary:
    We have function definitions for Python with params and return types.
    The params are an array each location of xref names and spans.
    We have to look those up to get a declaration we can turn into a symbol id.
    
    Following the Hack implementation use the ppr annotation mechanism to track locations of xref links as we process the signature.
    
    We bulk fetch each array of Python.Name facts to convert to python.Declaration facts via python.DeclarationWithName (it would be good to avoid this and just store python.Declaration facts in params natively).
    
    We share some of this implementation with Hack, so we can actually float a bit of this out now.
    
    Next: we have to lift those declarations into symbol ids and return them with their spans through thrift.
    
    A signature for a function will look something like this before rendering:
    ```
    Just (Function NotAsync (Name "__init__") [Parameter (Name "self") Nothing Nothing [],Parameter (Name "config") Nothing (Just (PyType "Config")) [(Declaration_cls (ClassDeclaration {classDeclaration_id = 88409077, classDeclaration_key = Just (ClassDeclaration_key {classDeclaration_key_name = Name {name_id = 16559587, name_key = Just "dataswarm.config.Config"}, classDeclaration_key_bases = Nothing})}),ByteSpan {byteSpan_start = Nat {unNat = 0}, byteSpan_length = Nat {unNat = 6}})],Parameter (Name "session_id") Nothing (Just (PyType "int")) [(Declaration_cls (ClassDeclaration {classDeclaration_id = 12158157, classDeclaration_key = Just (ClassDeclaration_key {classDeclaration_key_name = Name {name_id = 11505538, name_key = Just "int"}, classDeclaration_key_bases = Nothing})}),ByteSpan {byteSpan_start = Nat {unNat = 0}, byteSpan_length = Nat {unNat = 3}})],Parameter (Name "is_shadow") Nothing (Just (PyType "bool")) [],Parameter (Name "dr_recovery") Nothing (Just (PyType "bool")) []] (Just (PyType "None")))
    ```
    
    https://pxl.cl/2vWlv
    
    Reviewed By: simonmar
    
    Differential Revision: D43864201
    
    fbshipit-source-id: 4af9e03530716b94566192aa14b0d0d4103d92a0

--- glean/glass/Glean/Glass/Pretty/Python.hs
+++ glean/glass/Glean/Glass/Pretty/Python.hs
@@ -13,17 +13,22 @@ module Glean.Glass.Pretty.Python
     prettyPythonSignature
   ) where
 
+import Data.Maybe
+import Data.Map.Strict ( Map )
+import qualified Data.Map.Strict as Map
 import Data.Text ( Text, takeWhileEnd )
 import Data.Text.Prettyprint.Doc
 
 import qualified Glean
 import Glean.Angle as Angle
 import qualified Glean.Haxl.Repos as Glean
-import Glean.Schema.CodePython.Types as Python ( Entity(..) )
-import qualified Glean.Schema.Python.Types as Python
 import Glean.Util.ToAngle ( ToAngle(toAngle) )
 import Glean.Glass.Types ( SymbolId )
-import Glean.Glass.Utils ( fetchDataRecursive )
+import Glean.Glass.Utils
+
+import Glean.Schema.CodePython.Types as Python ( Entity(..) )
+import qualified Glean.Schema.Python.Types as Python
+import qualified Glean.Schema.Src.Types as Src
 
 -- Type of Python entity signatures to capture the the subset we generate
 -- We use this to separate the processing of the Glean type from the text
@@ -36,18 +41,28 @@ data Definition
  -- Variable
  -- Import
  -- Module
+ deriving Show
 
-data Parameter = Parameter !Name (Maybe ExprText) !(Maybe PyType)
+data Parameter = Parameter !Name (Maybe ExprText) !(Maybe PyType) XRefs
+ deriving Show
 
 -- modifiers
-data AsyncModifier = Async | NotAsync deriving (Eq)
+data AsyncModifier = Async | NotAsync
+ deriving (Eq, Show)
 
 newtype ExprText = ExprText Text
+ deriving Show
 
 -- names
 newtype Name = Name Text
+ deriving Show
 
 newtype PyType = PyType Text
+ deriving Show
+
+type XRefs = [(Python.Declaration, Src.ByteSpan)]
+
+type Ann = Maybe Python.Declaration
 
 prettyPythonSignature
   :: LayoutOptions
@@ -81,8 +96,15 @@ fromFunctionDefinition def = do
   returnTy <- case mReturnTy of
     Nothing -> pure Nothing
     Just tyInfo -> Just <$> fromTypeInfo tyInfo
+
+  paramsAndXRefs <- mapM (\x -> case Python.parameter_typeInfo x of
+    Nothing -> pure (x, [])
+    Just ti -> (x,) <$> fetchDeclsByNames (Python.typeInfo_xrefs ti)
+    ) pyParams
+
   nameStr <- Glean.keyOf name
-  params <- mapM fromParameter pyParams
+
+  params <- mapM fromParameter paramsAndXRefs
   return $ Function
     (if async then Async else NotAsync)
     (trimModule nameStr)
@@ -100,22 +122,24 @@ fromTypeInfo  :: Python.TypeInfo -> Glean.RepoHaxl u w PyType
 fromTypeInfo Python.TypeInfo{..} = PyType <$> Glean.keyOf
   typeInfo_displayType
 
-fromParameter :: Python.Parameter -> Glean.RepoHaxl u w Parameter
-fromParameter Python.Parameter{..} = do
+fromParameter
+   :: (Python.Parameter, [(Python.Declaration, Src.ByteSpan)])
+   -> Glean.RepoHaxl u w Parameter
+fromParameter (Python.Parameter{..}, xrefs) = do
   nameStr <- Glean.keyOf parameter_name
   tyInfo <- case parameter_typeInfo of
     Nothing -> return Nothing
     Just ty -> Just <$> fromTypeInfo ty
-  return $ Parameter (Name nameStr) (ExprText <$> parameter_value) tyInfo
+  return $ Parameter (Name nameStr) (ExprText <$> parameter_value) tyInfo xrefs
 
-pprDefinition :: Definition -> Doc ()
+pprDefinition :: Definition -> Doc Ann
 -- empty param case
 pprDefinition (Function async name [] returnTy) =
   hcat [
    pprAsync async, "def" <+> pprName name, "()",
    case returnTy of
       Nothing -> emptyDoc
-      Just ty -> space <> "->" <+> pprType ty
+      Just ty -> space <> "->" <+> pprTypeXRefs ty []
   ]
 -- full param list
 pprDefinition (Function async name params returnTy) =
@@ -126,28 +150,59 @@ pprDefinition (Function async name params returnTy) =
     )),
     rparen <+> case returnTy of
       Nothing -> emptyDoc
-      Just ty -> "->" <+> pprType ty
+      Just ty -> "->" <+> pprTypeXRefs ty []
     ]
 
-pprAsync :: AsyncModifier -> Doc ()
+pprAsync :: AsyncModifier -> Doc Ann
 pprAsync Async = "async" <> space
 pprAsync _ = emptyDoc
 
-pprParam :: Parameter -> Doc ()
-pprParam (Parameter name _mDefValue mty) = hcat
+pprParam :: Parameter -> Doc Ann
+pprParam (Parameter name _mDefValue mty xrefs) = hcat
   [ pprName name
   , case mty of
     Nothing -> emptyDoc
-    Just ty -> colon <+> pprType ty
+    Just ty -> colon <+> pprTypeXRefs ty xrefs
   ]
 
-pprName :: Name -> Doc ()
+pprName :: Name -> Doc Ann
 pprName (Name name) = pretty name
 
-pprType :: PyType -> Doc ()
-pprType (PyType ty) = pretty ty
+pprTypeXRefs :: PyType -> XRefs -> Doc Ann
+pprTypeXRefs (PyType ty) xrefs =
+    mconcat $ (\(frag, ann) -> annotate ann $ pretty frag) <$>
+      splitString ty spans
+  where
+    spans = map (\(ann, Src.ByteSpan{..}) ->
+               (ann, fromIntegral (Glean.fromNat byteSpan_start)
+                   , fromIntegral (Glean.fromNat byteSpan_length))) xrefs
 
-angleDeclToDef:: Angle Python.Declaration -> Angle Python.Definition
+fetchDeclsByNames
+  :: [Python.XRefViaName]
+  -> Glean.RepoHaxl u w [(Python.Declaration,Src.ByteSpan)]
+fetchDeclsByNames [] = pure []
+fetchDeclsByNames xrefs = do
+  result <- searchRecursiveWithLimit maxXRefs (angleDeclsByNames names)
+  decls <- mapM Glean.keyOf result
+  return $ mapMaybe (\Python.DeclarationWithName_key{..} ->
+    case Map.lookup declarationWithName_key_name xrefTable of
+      Nothing -> Nothing
+      Just span -> Just (declarationWithName_key_declaration, span)
+    ) decls
+  where
+    maxXRefs = Just 10
+
+    xrefTable :: Map Python.Name Src.ByteSpan
+    xrefTable = Map.fromList
+      [ (xRefViaName_target, xRefViaName_source)
+      | Python.XRefViaName{..} <- xrefs
+      ]
+
+    names :: [Glean.IdOf Python.Name]
+    names = map Glean.getId (Map.keys xrefTable)
+
+
+angleDeclToDef :: Angle Python.Declaration -> Angle Python.Definition
 angleDeclToDef decl = var $ \(def :: Angle Python.Definition) ->
   def `where_` [
     wild .= predicate @Python.DeclarationDefinition (
@@ -157,6 +212,14 @@ angleDeclToDef decl = var $ \(def :: Angle Python.Definition) ->
       end
     )
   ]
+
+-- Bulk convert each name to its definition entity
+angleDeclsByNames
+  :: [Glean.IdOf Python.Name] -> Angle Python.DeclarationWithName
+angleDeclsByNames names = predicate @Python.DeclarationWithName $
+  rec $
+    field @"name" (elementsOf (array (map (asPredicate . factId) names)))
+  end
 
 -- | we could use the sname here to lookup the associated decl fact
 -- as an xref in the type signature (c.f how Hack does this)
