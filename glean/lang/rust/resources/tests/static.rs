cfec65b370f84f8d3a9377c7642a8793406d234b
Don Stewart <dons@meta.com>
2023-05-03 03:27:07 -0700

    Remove the -Zsave-analysis indexer
    Summary:
    The flag itself no longer exists in rustc and this indexer never quite worked for our use cases.
    We will use rust-analyzer instead, which has SCIP and LSIF support already.
    
    -  D45292993 removed the build flavor we relied on
    - see  https://github.com/rust-lang/rust/issues/43606 for removal
    
    Reviewed By: simonmar
    
    Differential Revision: D45516746
    
    fbshipit-source-id: 684117415c5daebca7656c330592f1362781ab1c

--- glean/lang/rust/resources/tests/static.rs
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
-pub static FOO: i32 = 12;
