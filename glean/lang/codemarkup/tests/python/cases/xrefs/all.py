01bb2eb28fdfa1e3c21df85ef14a17de0724e5a7
Michael Podtserkovskii <michaelpo@meta.com>
2023-05-23 04:48:59 -0700

    Resolve location of re-exported symbols
    Summary:
    The problem we're solving https://fb.workplace.com/groups/5464462933674551/permalink/6081875638599941/
    
    The root cause
    - The class is defined in `libcst/matchers/_visitors.py`
    - Then it imported in `libcst/matchers/__init__.py`
    - Then a client code may import this class either from `_visitors.py` or from `__init__.py`
    - Then Glass trying to resolve location of an entity , but this entity doesn't have a location, because it's a fake entity, the real entity have another fqname
    
    How is that possible?
    Any module m1 that imports any symbol from module m2 automatically re-export this symbol and there's no limit on the depth of re-exporting
    
    So this example is totally legal
    ```
    # file a.py
    def say_hello():
        print("hello")
    
    # file b.py
    from a import say_hello
    
    # file c.py
    from b import say_hello
    
    # file main.py
    from c import say_hello
    say_hello()
    
    # terminal
    $ python3 main.py
    "hello from a"    # prints hello from a.py
    ```
    
    In this implementation
    - We traverse names until we find "original" one (actuall max depth of traversal = 5)
    - Return location of "original" declaration, not a fake one
    
    Differential Revision: D45169259
    
    fbshipit-source-id: d497af8828805d118d3a6d0a3db9612cb0a91e9a

--- glean/lang/codemarkup/tests/python/cases/xrefs/all.py
+++ glean/lang/codemarkup/tests/python/cases/xrefs/all.py
@@ -4,7 +4,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from big_lib import big_func, big_var
+from big_lib import BaseClass, big_func, big_var
 from lib import HelperClass as Helper
 
 var = 123
@@ -18,7 +18,7 @@ def fn():
     pass
 
 
-class LocalClass:
+class LocalClass(BaseClass):
     pass
 
 
--- glean/lang/codemarkup/tests/python/cases/xrefs/big_lib/__init__.py
+++ glean/lang/codemarkup/tests/python/cases/xrefs/big_lib/__init__.py
@@ -4,7 +4,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-# flake8: noqa
+from big_lib._inner import BaseClass
 
 big_var = 0
 
@@ -22,3 +22,6 @@ def big_func():
 @staticmethod
 async def func_sleep():
     return 1
+
+
+__all__ = ["big_func", "big_var", "func_sleep", "BaseClass"]
