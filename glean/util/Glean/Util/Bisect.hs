6c49055d16f1c29712251d2cca0a848dc72bf927
Simon Marlow <smarlow@meta.com>
2023-05-25 00:50:08 -0700

    Run buck2 bxl commands in parallel
    Summary:
    `--buck2-concurrency=N --chunk-cdbs=M --threads=T`
    
    Runs N buck2 bxl commands concurrently each processing M
    targets. Writing the facts happens concurrently in T threads.
    
    Each buck2 will do its own bisection if there's a failure.
    
    I tried once with N=1000 and killed the buck daemon. N=2 seems to work
    OK with M=512.
    
    Reviewed By: malanka
    
    Differential Revision: D46110535
    
    fbshipit-source-id: 895080ed070cd84308ef941dd1fd95c46117f414

--- glean/util/Glean/Util/Bisect.hs
+++ glean/util/Glean/Util/Bisect.hs
@@ -7,15 +7,16 @@
 -}
 
 module Glean.Util.Bisect (
-  Bisect(..), BisectM, execBisectM, bisect
+  Bisect(..), BisectM, execBisectM, execBisectRefM, bisect
 ) where
 
 import Control.Applicative
 import Control.Monad
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Except (ExceptT, runExceptT, throwE)
-import qualified Control.Monad.Trans.State.Strict as State
+import Control.Monad.Trans.Reader
 import Control.Monad.Trans.Class (lift)
+import Data.IORef
 
 data Bisect a b err = Bisect
   { bisectProcess :: [a] -> IO (Either err b)
@@ -25,7 +26,7 @@ data Bisect a b err = Bisect
   , bisectSplitting :: Int -> Int -> [a] -> IO ()
   }
 
-newtype BisectM err a = BisectM (ExceptT err (State.StateT Int IO) a)
+newtype BisectM err a = BisectM (ExceptT err (ReaderT (IORef Int) IO) a)
   deriving(Functor, Applicative, Alternative, Monad, MonadPlus, MonadIO)
 
 -- | Run a bisect operation
@@ -36,8 +37,19 @@ execBisectM
   -> BisectM err a
      -- ^ e.g. a call to 'bisect'
   -> IO (Either err a)
-execBisectM allowed (BisectM action) =
-  State.evalStateT (runExceptT action) allowed
+execBisectM allowed (BisectM action) = do
+  ref <- newIORef allowed
+  runReaderT (runExceptT action) ref
+
+-- | Run a bisect operation
+execBisectRefM
+  :: IORef Int
+     -- ^ Number of failures that we will bisect down the failing item
+     -- and continue.
+  -> BisectM err a
+     -- ^ e.g. a call to 'bisect'
+  -> IO (Either err a)
+execBisectRefM ref (BisectM action) = runReaderT (runExceptT action) ref
 
 bisect
   :: Bisect a b err
@@ -51,18 +63,20 @@ bisect !bis xs = BisectM $ attempt 0 (length xs) xs
       case r of
         Right x -> return [x]
         Left err -> do
-          e <- lift State.get
+          r <- lift ask
           case xs of
-            [x] -> liftIO $ bisectFailed bis err x
-            _ -> return ()
-          if e > 0
-            then split k n xs
-            else throwE (err,xs)
+            [x] -> do
+              liftIO $ bisectFailed bis err x
+              e <- liftIO $ atomicModifyIORef' r (\x -> (x, max 0 (x-1)))
+              when (e == 0) $ throwE (err,xs)
+              return []
+            _otherwise -> do
+              e <- liftIO $ readIORef r
+              if e > 0
+                then split k n xs
+                else throwE (err,xs)
 
     split _ _ [] = return []
-    split _ _ [_] = do
-      lift $ State.modify' (subtract 1)
-      return []
     split k n xs = do
       liftIO $ bisectSplitting bis k n xs
       (++) <$> attempt k m ys <*> attempt (k+m) (n-m) zs
