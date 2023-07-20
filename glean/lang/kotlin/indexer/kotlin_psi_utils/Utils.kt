83107937b36fab7bd650e71aded4840b2e8cef39
Don Stewart <dons@meta.com>
2023-07-20 04:50:50 -0700

    Port path cleaner from Java to Kotlin
    Summary: Reimplementation of the Java indexer's improved path cleaner. So we don't have so many invalid src.File path facts.
    
    Reviewed By: phlalx
    
    Differential Revision: D47580656
    
    fbshipit-source-id: faa75f1ddd96fee6052eafc7040eae9fbefb3a42

--- glean/lang/kotlin/indexer/kotlin_psi_utils/Utils.kt
+++ glean/lang/kotlin/indexer/kotlin_psi_utils/Utils.kt
@@ -8,20 +8,12 @@
 
 package glean.lang.kotlin.indexer.kotlin_psi_utils
 
-private const val FBSOURCE = "fbsource"
+private val FBSOURCE = "^/.*[/-]fbsource/".toRegex()
 
+private val NOISE = "^[0-9]+-[0-9]+/".toRegex()
+
+// Reimplementation of java-alpha indexer Utils.java path normalizer
 fun normalizePath(path: String): String {
-  // Paths may contain fbsource more than once, fbsource-, or, number dash directory
-  var fbSourceStartIndex: Int = path.indexOf(FBSOURCE)
-  var pathWithoutFbSource = path
-  while (fbSourceStartIndex != -1) {
-    pathWithoutFbSource = pathWithoutFbSource.substring(fbSourceStartIndex + FBSOURCE.length)
-    fbSourceStartIndex = pathWithoutFbSource.indexOf(FBSOURCE)
-  }
-  val splitPath = pathWithoutFbSource.split("[0-9]*-[0-9]*".toRegex()).toTypedArray()
-  val cleanPath =
-      if (splitPath.size == 1) splitPath[0]
-      else if (splitPath.isEmpty()) path else java.lang.String.join("", *splitPath)
-  return if (cleanPath.startsWith(FBSOURCE)) cleanPath
-  else FBSOURCE + if (cleanPath.startsWith("//")) cleanPath.substring(1) else cleanPath
+  val cleanPath = FBSOURCE.replaceFirst(path, "")
+  return NOISE.replaceFirst(cleanPath, "")
 }
