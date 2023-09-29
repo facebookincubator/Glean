f6b00a809b8f8455ac2cdb52e4620a07a5551e23
Alena Chernikava <chernikava@meta.com>
2023-09-29 09:59:50 -0700

    improve logging
    Summary:
    https://fburl.com/scuba/glean_glass_server/f9uhs1zx
    {F1101361396}
    We can see that repo names are in different order creating diffrent rows even if they represent the same sets
    
    Reviewed By: pepeiborra
    
    Differential Revision: D49594410
    
    fbshipit-source-id: 2b91a78fc26d3ccbbbc9c1f7db315e1d84f7b60b

--- glean/glass/Glean/Glass/Logging.hs
+++ glean/glass/Glean/Glass/Logging.hs
@@ -44,6 +44,7 @@ import qualified Data.Text as Text
 import Data.List.NonEmpty ( NonEmpty(..), toList )
 import qualified Data.List.NonEmpty as NE
 import Glean.Glass.SnapshotBackend ( SnapshotStatus(..) )
+import Data.Function (on)
 
 instance ActionLog GleanGlassLogger where
   successLog = Logger.setSuccess True
@@ -240,9 +241,11 @@ instance LogRepo (NonEmpty (a, Glean.Repo)) where
 
 instance LogRepo (NonEmpty Glean.Repo) where
   logRepo (repo :| []) = logRepo repo
-  logRepo rs@(_ :| _) =
+  logRepo rs0@(_ :| _) =
     Logger.setRepoName (commas repo_name rs) <>
     Logger.setRepoHash (commas (Text.take 12 . repo_hash) rs)
+    where
+      rs = NE.sortBy (compare `on` Glean.repo_name) rs0
 
 commas :: (Glean.Repo -> Text) -> NonEmpty Glean.Repo -> Text
 commas f = Text.intercalate "," . map f . toList
@@ -334,10 +337,12 @@ instance LogError (NonEmpty (a, Glean.Repo)) where
 
 instance LogError (NonEmpty Glean.Repo) where
   logError (repo :| []) = logError repo
-  logError rs =
+  logError rs0 =
     ErrorLogger [] (NE.toList rs) $
       Errors.setRepoName (commas repo_name rs) <>
       Errors.setRepoHash (commas (Text.take 12 . repo_hash) rs)
+    where
+    rs = NE.sortBy (compare `on` Glean.repo_name) rs0
 
 -- sometimes we return more than just the repo result
 instance LogError (a,Glean.Repo) where
