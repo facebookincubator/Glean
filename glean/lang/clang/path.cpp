4cd296a5d7c4c3ba7cb55a1b0750b89a24e2c03a
Simon Marlow <smarlow@meta.com>
2023-10-12 02:58:39 -0700

    Expand symlinks more selectively
    Summary:
    We had an issue where
    - the path is a symlink inside buck-out
    - pointing to a path inside root
    - containing a symlink to /mnt/gvfs
    - so we didn't resolve the original buck-out path because it resolved
    to a location outside root
    
    Instead we want to be more careful about following symlinks: follow
    symlinks iteratively and stop if the symlink points outside of root.
    
    Note that this doesn't try to follow symlinks contained within the
    path, only when the whole path points to a symlink. I don't know
    whether that's a problem or not.
    
    Reviewed By: p0
    
    Differential Revision: D47596061
    
    fbshipit-source-id: b8e9b4c109d478eb849a35cad15f77c1cb42023b

--- glean/lang/clang/path.cpp
+++ glean/lang/clang/path.cpp
@@ -10,6 +10,7 @@
 
 #include <algorithm>
 #include <cassert>
+#include <glog/logging.h>
 
 namespace facebook {
 namespace glean {
@@ -62,6 +63,53 @@ std::filesystem::path goodPath(
       return path;
     }
   }
+}
+
+std::filesystem::path followSymlinksInsideRoot(
+    std::filesystem::path root, std::filesystem::path source) {
+  CHECK(source.is_relative());
+
+  std::filesystem::path path; // current path
+  std::filesystem::path tail = source; // remaining path
+
+  for (auto next = tail.begin(); next != tail.end(); ) {
+    auto component = *next;
+    auto cur = root / path / component;
+    if (std::filesystem::is_symlink(cur)) {
+      auto target = std::filesystem::read_symlink(cur);
+      auto resolved = goodPath(root, path / target);
+      if (resolved.is_relative()) {
+        // append the remaining tail to the symlink target
+        for (auto it = ++next; it != tail.end(); it++) {
+          target = target / *it;
+        }
+        tail = target;
+        next = tail.begin();
+        continue;
+      } else {
+        // we're not resolving this symlink because it points outside the root
+        // let's try canonicalising the whole path just in case that gets us
+        // back into the root.
+        path /= component;
+        for (auto it = ++next; it != tail.end(); it++) {
+          path /= *it;
+        }
+        std::error_code ec;
+        auto canon = std::filesystem::canonical(root / path, ec);
+        if (!ec) {
+          canon = goodPath(root, canon);
+          if (canon.is_relative()) {
+            return canon;
+          }
+        }
+        return path;
+      }
+    } else {
+      path = goodPath(root, path / component);
+      next++;
+    }
+  }
+  return path;
 }
 
 }
