35610ffa43000dd3b775c1aea95ad2b9f1c7d53a
abhishek <tiwari.abhishektiwari23@gmail.com>
2023-10-18 23:28:41 -0700

    wrapper for python-scip

---
+++ glean/lang/python-scip/Glean/python.hs
@@ -0,0 +1,46 @@
+{-
+  Copyright (c) Meta Platforms, Inc. and affiliates.
+  All rights reserved.
+
+  This source code is licensed under the BSD-style license found in the
+  LICENSE file in the root directory of this source tree.
+-}
+
+{ -# LANGUAGE ApplicaPtiveDo #-}
+module Glean.Indexer.PythonScip ( indexer ) where
+
+import Options.Applicative
+
+import Glean.Indexer
+import Glean.Indexer.SCIP
+import Glean.Indexer.SCIP (derive)
+import Glean.SCIP.Driver as SCIP
+
+newtype PythonScip = PythonScip
+    { pythonAnalyzerBinary :: FilePath       
+    }
+
+options :: Parser PythonScip
+options = do
+    pythonAnalyzerBinary <- strOption $
+        long "python-analyzer" <>
+        value "python-analyzer" <>
+        help "path to python-analyzer binary"
+    return PythonScip{..}
+
+indexer :: Indexer PythonScip
+indexer = Indexer {
+    indexerShortName = "python-scip"
+    indexerDescription = "Index Python code with `python-analyzer scip`"
+    indexerOptParser = options
+    indexerRun = \PythonScip{..} backend repo IndexerParams{..} -> do
+        val <- SCIP.runIndexer ScipIndexerParams {
+            scipBinary = pythonAnalyzerBinary,
+            scipArgs = const [ "scip", "." ],
+            scipRoot = indexerRoot,
+            scipWritesLocal = True,
+            scipLanguage = Just SCIP.Python
+        }
+        sendJsonBatches backend repo (pythonAnalyzerBinary <> "/scip") val
+        derive backend repo
+}
