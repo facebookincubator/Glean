ade39b4529d9f434f10bae6b76c50e9dfb057a24
abhishek <tiwari.abhishektiwari23@gmail.com>
2023-11-22 01:55:03 -0800

    Dotnet test, example and query files (#396)
    Summary: Pull Request resolved: https://github.com/facebookincubator/Glean/pull/396
    
    Test Plan:
    Imported from GitHub, without a `Test Plan:` line.
    
    This compiles:
    > buck2 build @//mode/opt //glean/lang/dotnet-scip/tests:tests
    
    Reviewed By: simonmar
    
    Differential Revision: D51518733
    
    Pulled By: donsbot
    
    fbshipit-source-id: 265bd55f101fc63204840eb18a76cd83a30b8b35

---
+++ glean/lang/dotnet-scip/tests/cases/xrefs/Program.cs
@@ -0,0 +1,68 @@
+﻿/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+using System;
+
+public class Mammal
+{
+	private int numberOfLegs = 0;
+	private string specie = string.Empty;
+
+    public Mammal(){
+		specie = "Unknown";
+	}
+    
+	public Mammal(string specie, int numberOfLegs)
+	{
+		this.numberOfLegs = numberOfLegs;
+		this.specie = specie;
+	}
+
+	public int NumberOfLegs
+	{
+		get
+		{
+			return numberOfLegs;
+		}
+	}
+
+	public string Specie
+	{
+		get
+		{
+			return specie;
+		}
+	}
+}
+
+public class Dog : Mammal
+{
+	public Dog() : base("Dogs", 4)
+	{
+	}
+}
+
+public class Human : Mammal
+{
+	public Human() : base("Humans", 2)
+	{
+	}
+}
+
+class ExecuteInheritance {
+	static void Main(string[] args) {
+        var m = new Mammal();
+		Console.WriteLine($"{m.Specie} has {m.NumberOfLegs} legs");
+        
+        var d = new Dog();
+        Console.WriteLine($"{d.Specie} has {d.NumberOfLegs} legs");
+
+        var h = new Human();
+        Console.WriteLine($"{h.Specie} has {h.NumberOfLegs} legs");
+   }
+}
