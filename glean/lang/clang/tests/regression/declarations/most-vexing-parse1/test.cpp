51f0a1a818ac99c74a212556d365f0d507ef3300
Michael Park <mcypark@meta.com>
2024-01-04 03:13:52 -0800

    Add a new test for "most-vexing-parse" problem.
    Summary: Currently, a function declared within a function via most-vexing-parse is claimed to be within the global scope.
    
    Reviewed By: simonmar
    
    Differential Revision: D52421612
    
    fbshipit-source-id: 2763ed9018f5c128f2a78a668ce55d1ec0ecf355

---
+++ glean/lang/clang/tests/regression/declarations/most-vexing-parse1/test.cpp
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+struct mutex {};
+
+void f6() {
+  mutex mutex_;
+  int guard(mutex);
+}
