4426b277d51cf9a22f4bfc248d143425f75fa9fd
Michael Park <mcypark@meta.com>
2024-04-11 17:38:09 -0700

    Use `string()` instead of `native()` to avoid the `wstring` in Windows
    Summary: `path.native()` in Windows returns a `wstring`. I think we can get away with `path.string()` in these cases.
    
    Reviewed By: phlalx
    
    Differential Revision: D55515303
    
    fbshipit-source-id: 271d9cd1b8748be04dc1b0fd3b1dfe1b5b6470e9

--- glean/lang/clang/db.cpp
+++ glean/lang/clang/db.cpp
@@ -76,13 +76,13 @@ ClangDB::fileFromEntry(
 #endif
   }();
   if (!buffer) {
-    LOG(WARNING) << "Couldn't get MemoryBuffer for " << path.native();
+    LOG(WARNING) << "Couldn't get MemoryBuffer for " << path.string();
 #if GLEAN_FACEBOOK
     if (isBuckOutPath(path)) {
       return {};
     }
 #endif
-    const auto file = batch.fact<Src::File>(path.native());
+    const auto file = batch.fact<Src::File>(path.string());
     return std::pair{file, std::move(path)};
   }
   // compute the SHA1 digest of the file content and get the file size
@@ -94,7 +94,7 @@ ClangDB::fileFromEntry(
     path = replaceBuckOutHash(path, hash);
   }
 #endif
-  const auto file = batch.fact<Src::File>(path.native());
+  const auto file = batch.fact<Src::File>(path.string());
   const uint64_t size = entry.getSize();
   batch.fact<Digest::FileDigest>(file, Digest::Digest{hash, size});
 
