71b8ef3bbd2b07c75d21d57b240d4c29bfbcbfc7
Michael Park <mcypark@meta.com>
2024-04-30 08:41:59 -0700

    Unbreak missing `GleanDBAttrName`.
    Summary: Follow up to retire "attributes" from Glass.
    
    Reviewed By: pepeiborra
    
    Differential Revision: D56753417
    
    fbshipit-source-id: 30e8871de36bb42784847392db74a0a9a79df8c6

--- glean/glass/Glean/Glass/RepoMapping.hs
+++ glean/glass/Glean/Glass/RepoMapping.hs
@@ -18,20 +18,17 @@ import Data.Set (Set)
 import qualified Data.Set as Set
 import qualified Data.Map.Strict as Map
 
-import Glean.Glass.Base (
-  GleanDBAttrName(..), GleanDBName(..), RepoMapping(..) )
+import Glean.Glass.Base ( GleanDBName(..), RepoMapping(..) )
 import Glean.Glass.Types ( Language(..), RepoName(..) )
 
 getRepoMapping :: IO RepoMapping
 getRepoMapping = return RepoMapping
   { gleanIndices = gleanIndices_
-  , gleanAttrIndices = gleanAttrIndices_
   }
 
 fixedRepoMapping :: RepoMapping
 fixedRepoMapping = RepoMapping
   { gleanIndices = gleanIndices_
-  , gleanAttrIndices = gleanAttrIndices_
   }
 
 -- example: the open source react repo.
@@ -55,16 +52,11 @@ gleanIndices_ = Map.fromList
       ])
   ]
 
--- repos that contain symbol attributes
-gleanAttrIndices_ :: Map.Map GleanDBName [GleanDBAttrName]
-gleanAttrIndices_ = Map.empty
-
 -- | All the Glean db repo names we're aware of
 -- We will only be able to query members of this set
 allGleanRepos :: Set GleanDBName
 allGleanRepos = Set.fromList $
-  map fst (concat (Map.elems gleanIndices_)) ++
-  concatMap (map gleanAttrDBName) (Map.elems gleanAttrIndices_)
+  map fst (concat (Map.elems gleanIndices_))
 
 -- repos that are required
 gleanRequiredIndices :: Set.Set GleanDBName
