44d57b9bebfcc920d04197fb8ae17a99ec773bfc
Vladimir Makaev <vmakaev@meta.com>
2024-07-30 07:13:30 -0700

    Apply DOTNETFORMATCSHARP for fbsource/**
    Reviewed By: zertosh
    
    Differential Revision: D60428476
    
    fbshipit-source-id: db7094ef056e24b5b1ad8b86fe3e3a9d70478d39

--- glean/lang/dotnet-scip/tests/cases/xrefs/Program.cs
+++ glean/lang/dotnet-scip/tests/cases/xrefs/Program.cs
@@ -10,59 +10,62 @@ using System;
 
 public class Mammal
 {
-	private int numberOfLegs = 0;
-	private string specie = string.Empty;
+    private int numberOfLegs = 0;
+    private string specie = string.Empty;
 
-    public Mammal(){
-		specie = "Unknown";
-	}
-    
-	public Mammal(string specie, int numberOfLegs)
-	{
-		this.numberOfLegs = numberOfLegs;
-		this.specie = specie;
-	}
+    public Mammal()
+    {
+        specie = "Unknown";
+    }
 
-	public int NumberOfLegs
-	{
-		get
-		{
-			return numberOfLegs;
-		}
-	}
+    public Mammal(string specie, int numberOfLegs)
+    {
+        this.numberOfLegs = numberOfLegs;
+        this.specie = specie;
+    }
 
-	public string Specie
-	{
-		get
-		{
-			return specie;
-		}
-	}
+    public int NumberOfLegs
+    {
+        get
+        {
+            return numberOfLegs;
+        }
+    }
+
+    public string Specie
+    {
+        get
+        {
+            return specie;
+        }
+    }
 }
 
 public class Dog : Mammal
 {
-	public Dog() : base("Dogs", 4)
-	{
-	}
+    public Dog() : base("Dogs", 4)
+    {
+    }
 }
 
 public class Human : Mammal
 {
-	public Human() : base("Humans", 2)
-	{
-	}
+    public Human() : base("Humans", 2)
+    {
+    }
 }
 
-class ExecuteInheritance {
-	static void Main(string[] args) {
+class ExecuteInheritance
+{
+    static void Main(string[] args)
+    {
         var m = new Mammal();
-		Console.WriteLine($"{m.Specie} has {m.NumberOfLegs} legs");
-        
+        Console.WriteLine($"{m.Specie} has {m.NumberOfLegs} legs");
+
         var d = new Dog();
         Console.WriteLine($"{d.Specie} has {d.NumberOfLegs} legs");
 
         var h = new Human();
         Console.WriteLine($"{h.Specie} has {h.NumberOfLegs} legs");
-   }
+    }
 }
