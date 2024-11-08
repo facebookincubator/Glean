ef86cd961f587b3a2e3e647d70e03c023c8fb9bc
Don Stewart <dons@meta.com>
2024-11-07 18:52:06 -0800

    Initial kind-based generic search API
    Summary:
    This one should be compatible with existing clients.
    If there's a kind filter set, we'll pass it through to the search.code layer, and if the language supports it, dispatch to kind-specific tables.
    
    This should let us opt in languages one by one, and not require changing existing clients.
    
    Examples
    
    - find 100 classes starting with "f"
    ```
    
    fbsource> search.code.SearchByNameKindAndLanguage { name = "f".. , language = Cpp, kinds = { just = Class_}, entity = E } ; E
    
    100 results, 100 facts, 2.46ms, 4028736 bytes, 6661 compiled bytes
    Facts searched:
               cxx1.RecordDeclarationClass.5 : 98
                     cxx1.RecordDefinition.5 : 77
    ```
    
    Is there any namespace or class called "Bento" ? Immediately negative.
    ```
    fbsource> search.code.SearchByNameKindAndLanguage { name = "Bento" , language = Cpp, kinds = { just = Class_ | Namespace }, entity = E } ; E
    
    0 results, 0 facts, 2.51ms, 5629360 bytes, 13179 compiled bytes
    ```
    
    is there anything that matches "bento" in container space?
    ```
    fbsource> search.code.SearchByNameKindAndLanguage { name = "bento" , language = Cpp, entity = E } ; E
    { "id": 1497069299, "key": { "cxx": { "defn": { "namespace_": { "id": 361949103 } } } } }
    { "id": 1497069300, "key": { "cxx": { "defn": { "namespace_": { "id": 361933109 } } } } }
    { "id": 1497069301, "key": { "cxx": { "defn": { "namespace_": { "id": 818692353 } } } } }
    { "id": 1497069302, "key": { "cxx": { "defn": { "namespace_": { "id": 361949105 } } } } }
    { "id": 1497069303, "key": { "cxx": { "defn": { "namespace_": { "id": 361933176 } } } } }
    
    5 results, 5 facts, 3.81ms, 3749232 bytes, 9931 compiled bytes
    Facts searched:
                  cxx1.NamespaceDefinition.5 : 5
           cxx1.NamespaceDeclarationByName.5 : 5
    ```
    
    Still todo:
    - scope search with kinds
    - lower case conversions
    - a few more C++ types to test the whole thing from glass end to end.
    
    testing:
    ```
    glean  --db-root=d --schema=fbsource/fbcode/glean/schema/source create  --update-schema-for-stacked  --stacked=fbsource/add4d8f59e1cdb40389bd7af2d8d64db9ce51fff --db=fbsource/2
    
    glean  --db-root=d --schema=fbsource/fbcode/glean/schema/source derive --repo=fbsource/2 cxx1.RecordDeclarationStruct cxx1.RecordDeclarationClass cxx1.RecordDeclarationUnion cxx1.NamespaceDeclarationByName -j16
    ```
    
    Reviewed By: josefs
    
    Differential Revision: D65407462
    
    fbshipit-source-id: f4ebd78ccc2d2ff6b9dbdd8eb0bd9dafa6c3e498

---
+++ glean/lang/codemarkup/tests/clang/xrefs/containers/test.cpp
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+namespace foo {
+
+struct S { int n; };
+
+union U
+{
+    int n;
+    double c;
+};
+
+enum E { e_a, e_b, e_c };
+
+}
