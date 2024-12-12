bfef488e385a60891018f68727056cd521ba50ed
Don Stewart <dons@meta.com>
2024-12-11 21:15:54 -0800

    Optimize python.QueryToScopeCase
    Summary:
    We want to ensure this is always "out to in" when constructing SNames. .. at least for very long SNames. For short ones, not sure.
    
    something recently changed to trigger the cost of this to blow up as it enumerates SNames in the opposite direction.
    
    Fix it by specifying an order and adding some perf tests.
    
    Before: would be a timeout:
    ```
    fbsource.fbcode.python>  search.python.QueryToScopeCase { ["gen_ai","genie_projects","offline_inference", "app","llama2","config"], false, _ }
    {
      "id": 1419228269,
      "key": {
        "query": [ "gen_ai", "genie_projects", "offline_inference", "app", "llama2", "config" ],
        "insensitive": false,
    ...
    
    1 results, 13 facts, 7464.64ms, 24105032 bytes, 12056 compiled bytes
    Facts searched:
                              python.SName.4 : 142544
                 python.SearchModuleByName.4 : 5
                               python.Name.4 : 1
                  python.SearchClassByName.4 : 1
    ```
    
    After:
    ```
    fbsource.fbcode.python> search.python.QueryToScopeCase { ["gen_ai","genie_projects","offline_inference", "app","llama2","config"], false, _ }
    {
      "id": 1419086769,
      "key": {
        "query": [ "gen_ai", "genie_projects", "offline_inference", "app", "llama2", "config" ],
        "scopeName": {
    ...
    
    1 results, 13 facts, 14.80ms, 28758344 bytes, 14170 compiled bytes
    Facts searched:
                              python.SName.4 : 313
                               python.Name.4 : 6
                 python.SearchModuleByName.4 : 5
                  python.SearchClassByName.4 : 1
    ```
    
    I think this lurking issue might also explain why there was only minor movement in python scope search p99, whereas name search dropped dramatically when the name tables switched over;
    
     {F1973781970}
    
    Reviewed By: phlalx
    
    Differential Revision: D67120662
    
    fbshipit-source-id: 3445dbf55d7d86dc8a82b25ade37b4e01edbcd25

--- glean/lang/codemarkup/tests/python/cases/search/some/foo.py
+++ glean/lang/codemarkup/tests/python/cases/search/some/foo.py
@@ -1,4 +1,8 @@
-# (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
 
 # flake8: noqa
 from .lib import a
@@ -14,3 +18,40 @@ def foo():
 
 
 foo_var = 1
+
+
+class In1:
+    class In2:
+        class In3:
+            class In4:
+                class In5:
+                    class In6:
+                        class In7:
+                            class In8:
+                                class In9:
+                                    def meth_9(self):
+                                        return none
+
+                                def meth_8(self):
+                                    return none
+
+                            def meth_7(self):
+                                return none
+
+                        def meth_6(self):
+                            return none
+
+                    def meth_5(self):
+                        return none
+
+                def meth_4(self):
+                    return none
+
+            def meth_3(self):
+                return none
+
+        def meth_2(self):
+            return none
+
+    def meth_1(self):
+        return none
