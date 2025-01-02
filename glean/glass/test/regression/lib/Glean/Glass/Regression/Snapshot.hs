08e442cac3892d8f9902ab6e78aca70dae09f0fd
Philippe Bidinger <pbidinger@meta.com>
2025-01-02 03:58:55 -0800

    use request option rather than feature flag
    Summary: use new request option introduced in D66724863 rather than feature flag
    
    Reviewed By: donsbot
    
    Differential Revision: D67713238
    
    fbshipit-source-id: 60b4240f59f9c93d22ff28be2deddb22a0579184

--- glean/glass/test/regression/lib/Glean/Glass/Regression/Snapshot.hs
+++ glean/glass/test/regression/lib/Glean/Glass/Regression/Snapshot.hs
@@ -179,17 +179,12 @@ mkTest cfgReplace get name qfile tempDir = TestLabel name $ TestCase $ do
       then copyFile actual expected
       else diff actual expected
 
-xlang :: RequestOptions -> RequestOptions
-xlang opts = opts { requestOptions_feature_flags = Just
-  (def { featureFlags_include_xlang_refs = Just True })
-}
-
 evalQuery :: Glass.Env -> FilePath -> Query -> FilePath -> IO ()
 evalQuery glassEnv qFile Query{..} oFile = case action of
   "documentSymbolListX" -> withObjectArgs qFile oFile args
-    (\req opts -> Glass.documentSymbolListX glassEnv req (xlang opts))
+    (\req opts -> Glass.documentSymbolListX glassEnv req opts)
   "documentSymbolIndex" -> withObjectArgs qFile oFile args
-    (\req opts -> Glass.documentSymbolIndex glassEnv req (xlang opts))
+    (\req opts -> Glass.documentSymbolIndex glassEnv req opts)
   "findReferenceRanges" -> withSymbolId oFile args
     (Glass.findReferenceRanges glassEnv)
   "symbolLocation" -> withSymbolId oFile args
