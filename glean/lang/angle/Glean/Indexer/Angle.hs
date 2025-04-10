64b55f21a088c65b53349c7f16ed51c2f3f41855
Julia Molin <juliamolin@meta.com>
2025-04-10 10:19:39 -0700

    build FileLines facts + add SrcSpan -> ByteSpan transformation
    Summary:
    * Building the FileLines facts
    * Adding a function SrcSpan -> ByteSpan
      * we need ByteSpan for code navigation
      * We already have some helper functions for this, if we have the fileLines info available (why these two changes are done together).
    
    Reviewed By: donsbot
    
    Differential Revision: D72003008
    
    fbshipit-source-id: f1d3cd0bf062d04d6b5062441b9ff3b197c499e3

--- glean/lang/angle/Glean/Indexer/Angle.hs
+++ glean/lang/angle/Glean/Indexer/Angle.hs
@@ -6,7 +6,7 @@
   LICENSE file in the root directory of this source tree.
 -}
 
-{-# LANGUAGE ApplicativeDo #-}
+{-# LANGUAGE ApplicativeDo, TypeApplications #-}
 
 module Glean.Indexer.Angle
   (main
@@ -14,20 +14,38 @@ module Glean.Indexer.Angle
 
 
 import Data.Default
+import qualified Data.ByteString.Char8 as BC
+import qualified Data.HashMap.Strict as HashMap
+import qualified Data.Text as Text
+import Control.Exception
 import Options.Applicative
 import Util.EventBase
+import Util.IO (listDirectoryRecursive)
 
 import Glean
+import Glean.Database.Config
+  (ProcessedSchema (..), processSchema, catSchemaFiles)
 import Glean.LocalOrRemote
 import Glean.Util.ConfigProvider
 import Glean.Impl.ConfigProvider
 import Glean.Schema.Builtin.Types (schema_id)
+import Glean.Indexer.Utils
+import Glean.Angle.Types
+import Glean.Schema.Types
+import qualified Glean.Schema.Src.Types as Src
+import qualified Glean.Schema.Src as Src (allPredicates)
+
 
 data Options = Options
   { optDir ::  String
   , optDb :: Glean.Repo
   , optService :: Service
   }
+
+-- Combining all info needed to build facts for an angle schema
+-- such that we can easily iterate them on a "per schema" basis.
+data AngleSchema =
+  AngleSchema SourceSchema ResolvedSchemaRef FilePath ToByteSpan
 
 opts :: ParserInfo Options
 opts = info (helper <*> parser) fullDesc
@@ -45,24 +63,81 @@ opts = info (helper <*> parser) fullDesc
       optService <- options
       return Options{..}
 
-buildFacts :: FactBuilder
-buildFacts = return () -- Not implemented yet
+buildFacts :: ProcessedSchema -> [SourceFileInfo] -> FactBuilder
+buildFacts ProcessedSchema{..} fileinfos = do
+  schemaFileInfo <- mapM buildFileLines fileinfos
+  let schemaFileInfoMap = toFileInfoMap schemaFileInfo
+      resolvedSchemaMap = toResolvedSchemaMap procSchemaResolved
 
-send :: Backend be => be -> Repo -> IO ()
-send be repo = do
-  withSender be repo refs def $ \sender ->
+  schemas <- mapM (\sSchema -> do
+    let name = sourceRefName $ schemaName sSchema
+    rSchema <- case HashMap.lookup name resolvedSchemaMap of
+      Just rSchema -> return rSchema
+      Nothing -> fail $ "Couldn't find resolved schema" <> show name
+    (file, toBs) <- case HashMap.lookup name schemaFileInfoMap of
+      Just SchemaFileInfo{..} -> return (filepath sourceFileInfo, toByteSpan)
+      Nothing -> fail $ "Couldn't find file info for schema " <> show name
+    return $ AngleSchema sSchema rSchema file toBs
+    ) $ srcSchemas procSchemaSource
+
+  mapM_ buildSchemaFacts schemas
+
+buildSchemaFacts :: AngleSchema -> FactBuilder
+buildSchemaFacts _schema = do
+  -- TODO: not implemented yet
+  return ()
+
+readSchemas :: FilePath -> IO ProcessedSchema
+readSchemas dir = do
+  files <- listDirectoryRecursive dir
+  bytestr <- catSchemaFiles files
+  case processSchema Nothing bytestr of
+    Left err -> throwIO $ ErrorCall err
+    Right schema -> return schema
+
+buildFileLines ::forall m. NewFact m => SourceFileInfo -> m SchemaFileInfo
+buildFileLines fileInfo = do
+  let SourceFileInfo _name filepath bytestr = fileInfo
+      byteLines = BC.lines bytestr
+      byteLinesWithNewLine = map (<> "\n") byteLines
+      byteFileLines = map (Glean.Nat . fromIntegral . BC.length)
+        byteLinesWithNewLine
+
+  file <- makeFact @Src.File (Text.pack filepath)
+  let fileLinesKey = Src.FileLines_key
+          { fileLines_key_file = file
+          , fileLines_key_lengths = byteFileLines
+          , fileLines_key_endsInNewline = True
+          , fileLines_key_hasUnicodeOrTabs = False
+          }
+  makeFact_ @Src.FileLines fileLinesKey
+
+  -- When writing the later facts, we need:
+  --   filepath
+  --   A function: SrcSpan -> ByteSpan.
+  let toByteSpan = fromSrcSpan fileLinesKey bytestr
+  return $ SchemaFileInfo fileInfo toByteSpan
+
+send :: Backend be => be -> ProcessedSchema -> Options -> IO ()
+send be schemas opts = do
+  -- pre-processing before building facts
+  fileInfos <- sourceFileInfos $ optDir opts
+
+  withSender be (optDb opts) refs def $ \sender ->
     withWriter sender def $ \writer ->
-      writeFacts writer buildFacts
+      writeFacts writer (buildFacts schemas fileInfos)
 
     where
       -- TODO: add predicates sent, e.g., [ Anglelang.allPredicates, Src., ..]
-      refs = []
+      refs = [Src.allPredicates]
 
 main :: IO ()
 main = do
-  withConfigOptions opts $ \(opts, cfgOpts) ->
+  withConfigOptions opts $ \(opts, cfgOpts) -> do
+    let service = optService opts
+    schemas <- readSchemas $ optDir opts
     withEventBaseDataplane $ \evb ->
-    withConfigProvider cfgOpts $ \(cfgAPI :: ConfigAPI) ->
-    withBackendWithDefaultOptions evb cfgAPI (optService opts) (Just schema_id)
-      $ \backend -> do
-        send backend $ optDb opts
+      withConfigProvider cfgOpts $ \(cfgAPI :: ConfigAPI) ->
+      withBackendWithDefaultOptions evb cfgAPI service (Just schema_id)
+        $ \backend -> do
+          send backend schemas opts
---
+++ glean/lang/angle/Glean/Indexer/Utils.hs
@@ -0,0 +1,89 @@
+{-
+  Copyright (c) Meta Platforms, Inc. and affiliates.
+  All rights reserved.
+
+  This source code is licensed under the BSD-style license found in the
+  LICENSE file in the root directory of this source tree.
+-}
+
+module Glean.Indexer.Utils
+  (toFileInfoMap,
+  toResolvedSchemaMap,
+  sourceFileInfos,
+  SchemaFileInfo(..),
+  SourceFileInfo(..),
+  ToByteSpan,
+  FileInfoMap,
+  fromSrcSpan,
+  ) where
+
+import qualified Data.ByteString as B
+import qualified Data.HashMap.Strict as HashMap
+import Control.Exception
+import System.FilePath
+import Util.IO ( listDirectoryRecursive )
+
+import Glean
+import Glean.Util.Range (srcRangeToByteRange, rangeToByteSpan)
+import qualified Glean.Schema.Src.Types as Src
+import Glean.Angle.Types
+import Glean.Angle.Parser (parseSchema)
+import Glean.Schema.Types
+
+data SourceFileInfo = SourceFileInfo {
+  name :: Name
+  , filepath :: FilePath
+  , bytestr :: B.ByteString
+}
+
+data SchemaFileInfo = SchemaFileInfo {
+  sourceFileInfo :: SourceFileInfo
+  , toByteSpan :: ToByteSpan
+}
+
+type ToByteSpan = SrcSpan -> Src.ByteSpan
+type FileInfoMap = HashMap.HashMap Name SchemaFileInfo
+
+-- Collect bytestrings from source code
+-- we need this to create the Src.FileLines,
+-- and for SrcSpan -> Src.ByteSpan conversion
+sourceFileInfos :: FilePath -> IO [SourceFileInfo]
+sourceFileInfos dir = do
+  files <- filter ((== ".angle") . takeExtension) <$> listDirectoryRecursive dir
+  sourceFileInfo <- mapM  (\file -> do
+    bytestr <- B.readFile file
+    case parseSchema bytestr of
+        Left err -> throwIO $ ErrorCall err
+        Right SourceSchemas{..} -> return $ map (\s -> do
+          let name = sourceRefName $ schemaName s
+          SourceFileInfo name file bytestr
+          )
+          srcSchemas) files
+  return $ concat sourceFileInfo
+
+toFileInfoMap :: [SchemaFileInfo] -> FileInfoMap
+toFileInfoMap fileInfos =
+  HashMap.fromList $ map (\schemaInfo ->
+    (name $ sourceFileInfo schemaInfo, schemaInfo)) fileInfos
+
+toResolvedSchemaMap :: ResolvedSchemas -> HashMap.HashMap Name ResolvedSchemaRef
+toResolvedSchemaMap schemas = HashMap.fromList $ map (\schema ->
+  (resolvedSchemaName schema, schema)) $ schemasResolved schemas
+
+fromSrcSpan :: Src.FileLines_key -> B.ByteString -> (SrcSpan -> Src.ByteSpan)
+fromSrcSpan filelineskey bytestr = do
+  let file = Src.fileLines_key_file filelineskey
+      rangeToByteRange = srcRangeToByteRange filelineskey bytestr
+      toByteSpan =  rangeToByteSpan . rangeToByteRange
+  toByteSpan . toSourceRange file
+
+toSourceRange :: Src.File -> SrcSpan -> Src.Range
+toSourceRange file SrcSpan{..} =
+  Src.Range {
+    range_file = file
+    , range_lineBegin = toNat $ locLine spanStart
+    , range_columnBegin = toNat $ locCol spanStart
+    , range_lineEnd = toNat $ locLine spanEnd
+    , range_columnEnd = toNat $ locCol spanEnd
+  }
+   where toNat = Glean.Nat . fromIntegral
