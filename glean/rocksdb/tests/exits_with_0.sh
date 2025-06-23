ddd525029bec3173cb03a3801fa68cd4010825b2
Nikita Patskov <patskovn@meta.com>
2025-06-23 06:31:15 -0700

    Properly configured glean ffi tests
    Summary: Using proper macroses for testing
    
    Differential Revision: D77025011
    
    fbshipit-source-id: cb56656a2b3c80144aaaa2e61a0ebe47e3c49e0d

---
+++ glean/rocksdb/tests/exits_with_0.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+# Simple script that exits with code 0
+# Can be run with arguments
+
+$1
