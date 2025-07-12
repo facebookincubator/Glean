38877a4bf00cb3ca2a6fe03863e61cebe57d0b25
Simon Marlow <marlowsd@gmail.com>
2025-07-12 14:11:42 -0700

    Use Tasty as the test runner in the OSS build (#555)
    Summary:
    NOTE: includes https://github.com/facebookincubator/Glean/issues/558
    
    This gives us
    * Parallel test runs
    * Test listing via `--list-tests`
    * Test selection via `-p <pattern>`
    
    Pull Request resolved: https://github.com/facebookincubator/Glean/pull/555
    
    Reviewed By: phlalx
    
    Differential Revision: D78210339
    
    Pulled By: pepeiborra
    
    fbshipit-source-id: 0578c239b6f631773acd1ec7e3a28db4ecb3ea23

--- glean/github/TestRunner.hs
+++ glean/github/TestRunner.hs
@@ -8,16 +8,36 @@
 
 module TestRunner (module TestRunner) where
 
-import Control.Monad
-import System.Exit
+import System.Environment (withArgs)
 import Test.HUnit
+import qualified Test.Tasty as Tasty
+import qualified Test.Tasty.HUnit.Adapter as Tasty
 
+-- | Wraps the HUnit test using tasty, so we get parallel test runs,
+-- test listing, and test selection.
 testRunner :: Test -> IO ()
-testRunner t = do
-  Counts{..} <- runTestTT t
-  when (errors + failures > 0) $ exitWith (ExitFailure 1)
+testRunner t =
+  Tasty.defaultMain $
+  Tasty.testGroup "test" $
+  Tasty.hUnitTestToTestTree t
 
 data TestAction = TestAction
 
+-- | This entry point is designed for combining the test runner's
+-- option parser with the program's own options. Ideally we would use
+-- this to combine Tasty's option parser with the program's parser,
+-- but Tasty's option parser depends on the `Test`, and we don't have
+-- that when parsing options (it might depend on the options), so
+-- there's a circular dependency.
+--
+-- Therefore we run Tasty's CLI parser with empty arguments. We can
+-- still use the environment variables to set Tasty options, e.g.
+--
+-- > TASTY_NUM_THREADS=12 cabal run my-test
+--
 testRunnerAction :: TestAction -> Test -> IO ()
-testRunnerAction _ = testRunner
+testRunnerAction _ t =
+  withArgs [] $
+  Tasty.defaultMain $
+  Tasty.testGroup "test" $
+  Tasty.hUnitTestToTestTree t
