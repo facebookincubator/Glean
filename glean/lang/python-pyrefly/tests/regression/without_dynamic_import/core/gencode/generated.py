92a39fff4a341fa105390b309bb9efc65a5e3524
Rubmary Rojas <rubmary@meta.com>
2025-10-31 10:44:58 -0700

    Regression tests for gencode fact
    Summary: Adding glean regression tests for gencode fact
    
    Reviewed By: aahanaggarwal
    
    Differential Revision: D85950293
    
    fbshipit-source-id: f828e60299abb5b5d63cd25813887e46f0c95e77

---
+++ glean/lang/python-pyrefly/tests/regression/without_dynamic_import/core/gencode/generated.py
@@ -0,0 +1,7 @@
+ # @generated SignedSource<<abcde123456>>
+ # @codegen-command : scripts/foo/bar
+ # @codegen-source FooConfig: this/is/the/Way.php
+ # @codegen-class : BarCodeGen
+
+def helloworld():
+    pass
---
+++ glean/lang/python-pyrefly/tests/regression/without_dynamic_import/core/gencode/source-without-name.py
@@ -0,0 +1,14 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+# @partially-generated SignedSource<<abcde123456>>
+# @codegen-command : scripts/foo/bar
+# @codegen-source : this/is/the/Way.php
+# @codegen-class : BarCodeGen
+
+
+def hiworld():
+    pass
