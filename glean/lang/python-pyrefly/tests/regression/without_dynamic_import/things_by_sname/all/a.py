a437c800b001f7be4b8787d590b59bd767fe3aef
Rubmary Rojas <rubmary@meta.com>
2025-10-31 10:44:58 -0700

    Things by sname regression tests
    Summary: As title
    
    Reviewed By: aahanaggarwal
    
    Differential Revision: D85955448
    
    fbshipit-source-id: 7596e46b9530c137637f01a19d6fd3ad85347a7d

---
+++ glean/lang/python-pyrefly/tests/regression/without_dynamic_import/things_by_sname/all/a.py
@@ -0,0 +1,21 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+# flake8: noqa
+
+from mod.b import bello
+
+
+hello = 1
+
+
+def func(param):
+    pass
+
+
+class Cls:
+    def meth(self, param):
+        pass
