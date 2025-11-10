4bbaf5aad8132572f4a8d2769e4f203614821198
Simon Hollis <simon.hollis@meta.com>
2025-11-10 08:46:08 -0800

    Add JSON ownership file output to cxx indexer
    Summary:
    # What
    Add JSON file output of ownership data from the Glean Cxx indexer
    
    # Details
    Each shard produces an additional output called `ownership_data`. This is a tree that contains CAS digests that point to one JSON file per indexing chunk. There is therefore `#shards * #chunks per shard` JSON files.
    
    # Why
    Two reasons:
    1) To obtain and analyse ownership range data for offline analysis
    2) As a stepping stone to fixing the `int32` range issue for ownership. Following diffs will propose solutions that may use this output file.
    
    Differential Revision: D86627024
    
    fbshipit-source-id: fefad11f05b89f05d2a038dd5ed29bae32f43918

--- glean/lang/clang/index.cpp
+++ glean/lang/clang/index.cpp
@@ -51,6 +51,10 @@ DEFINE_string(
     dump,
     "",
     "PATH where generated facts will be dumped instead of sending them to the Glean write server. You MUST specify --dump.");
+DEFINE_string(
+    ownership_dump,
+    "",
+    "PATH where ownership data will be dumped as JSON (optional)");
 DEFINE_string(work_file, "", "PATH to work file");
 DEFINE_string(task, "", "task id (for logging)");
 DEFINE_string(request, "", "request id (for logging)");
@@ -764,6 +768,32 @@ int main(int argc, char** argv) {
   }
 
   indexer.batch.logEnd();
+
+  // Write ownership data to JSON file if ownership flag is set and dump path is
+  // specified
+  // if (FLAGS_ownership && !FLAGS_ownership_dump.empty()) {
+  LOG_CFG(INFO, config) << "Writing ownership data to " << FLAGS_ownership_dump;
+  auto ownership_data = indexer.batch.base().serializeOwnership();
+
+  folly::dynamic ownership_json = folly::dynamic::object;
+  for (const auto& [unit_name, fact_id_ranges] : ownership_data) {
+    folly::dynamic ranges_array = folly::dynamic::array;
+    for (size_t i = 0; i < fact_id_ranges.size(); i += 2) {
+      ranges_array.push_back(
+          folly::dynamic::array(fact_id_ranges[i], fact_id_ranges[i + 1]));
+    }
+    ownership_json[unit_name] = ranges_array;
+  }
+
+  std::string json_str = folly::toJson(ownership_json);
+  if (!folly::writeFile(json_str, FLAGS_ownership_dump.c_str())) {
+    LOG_CFG(ERROR, config) << "Failed to write ownership data to "
+                           << FLAGS_ownership_dump;
+  } else {
+    LOG_CFG(INFO, config) << "Successfully wrote ownership data for "
+                          << ownership_data.size() << " units";
+  }
+  //}
 
   config.counters.fact_buffer_size->store(0);
   config.counters.fact_cache_size->store(0);
