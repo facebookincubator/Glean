ecb4e67f5aa9474411be76de0132e49fe6423d7f
Julia Molin <juliamolin@meta.com>
2025-12-10 07:18:10 -0800

    add cointainer aware memory reader in glean
    Summary:
    We need to read memory capacity to set e.g., disk-limit and rockdb cache size relative the memory size.
    
    **Container aware**
    * we don't stack tasks at the moment, but this will make it easier to do so in the future
    * it's still useful to be container aware, since it's possible that TW tasks are deployed with memory limit
    
    **OSS compat**
    The facebook version is using fb libraries, so a template module that returns Nothing will be used for OSS, but still open to implement if anyone wants to in the future
    
    Reviewed By: iamirzhan
    
    Differential Revision: D87988951
    
    fbshipit-source-id: 47efd9690485879116cd4522c61747974f0e4bad

---
+++ glean/util/Glean/Impl/MemoryReader.hs
@@ -0,0 +1,16 @@
+{-
+  Copyright (c) Meta Platforms, Inc. and affiliates.
+  All rights reserved.
+
+  This source code is licensed under the BSD-style license found in the
+  LICENSE file in the root directory of this source tree.
+-}
+
+module Glean.Impl.MemoryReader
+  (totalMemCapacity
+  ) where
+
+-- template for open source build
+-- not implemented yet
+totalMemCapacity :: Maybe Int
+totalMemCapacity = Nothing
