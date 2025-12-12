58f3d29a8fdd4864be9317d9f43e82f65519bfb6
Simon Marlow <marlowsd@gmail.com>
2025-12-12 11:50:13 -0800

    Fix glass-regression-flow test (#636)
    Summary:
    Removing the default `--write-root=test` in https://github.com/facebookincubator/Glean/issues/623 broke this test, I tried to update the test to use an empty prefix but couldn't get it to work so I'm just adding `--write-root=test` to fix it for now.
    
    Pull Request resolved: https://github.com/facebookincubator/Glean/pull/636
    
    Reviewed By: iamirzhan
    
    Differential Revision: D89059821
    
    Pulled By: phlalx
    
    fbshipit-source-id: 212ec4428bbe1fd3f916b763da32efe22228c463

--- glean/glass/test/regression/Glean/Glass/Regression/Flow/Main.hs
+++ glean/glass/test/regression/Glean/Glass/Regression/Flow/Main.hs
@@ -12,6 +12,6 @@ import System.Environment
 import qualified Glean.Glass.Regression.Flow as Glass
 
 main :: IO ()
-main = getArgs >>= \args -> withArgs (["--root", path] ++ args) Glass.main
+main = getArgs >>= \args -> withArgs (args ++["--root", path, "--write-root", "test"]) $ Glass.main
   where
     path = "glean/lang/codemarkup/tests/flow/cases/xrefs"
