2105c690d189839eb07eff54e4a2f251cf0eacc9
Julia Molin <juliamolin@meta.com>
2025-12-19 06:26:02 -0800

    refactor test
    Summary: will add more here, so this is just for readability
    
    Reviewed By: phlalx
    
    Differential Revision: D89382183
    
    fbshipit-source-id: 1ad69872e3b2e235490d5f403aaa3215e86bee21

--- glean/test/tests/StorageRocksDBTest.hs
+++ glean/test/tests/StorageRocksDBTest.hs
@@ -20,58 +20,64 @@ import TestRunner
 import Glean.Init
 import Glean.Util.Disk (getDiskSize)
 
+testNoDiskLimit :: Test
+testNoDiskLimit =
+  TestCase $ withSystemTempDirectory "rocksdb-test" $ \tmpDir -> do
+    storage <- RocksDB.newStorage tmpDir def
+    assertEqual "No disk limit is set" Nothing
+      (RocksDB.rocksMaxDiskSize storage)
+
+    fullDiskCapacity <- getDiskSize tmpDir
+    Just rocksTotalCapacity <- getTotalCapacity storage
+    assertEqual "rocks has full disk capacity" fullDiskCapacity
+      rocksTotalCapacity
+
+testDiskLimitWithRatio :: Test
+testDiskLimitWithRatio =
+  TestCase $ withSystemTempDirectory "rocksdb-test" $ \tmpDir -> do
+    let config = def
+          { ServerConfig.config_db_rocksdb_disk_mem_capacity_ratio_limit =
+              Just 20
+          }
+    storage <- RocksDB.newStorage tmpDir config
+    assertBool "Disk limit exist" $
+      isJust (RocksDB.rocksMaxDiskSize storage)
+
+testGetTotalCapacity :: Test
+testGetTotalCapacity =
+  TestCase $ withSystemTempDirectory "rocksdb-test" $ \tmpDir -> do
+    storage <- RocksDB.newStorage tmpDir def
+
+    -- limit is definitely smaller than the actual disk size
+    -- limit will be applied
+    let maxDiskSmall = 2 * 1024 * 1024 * 1024 -- 2 GB
+        storageMocked = storage
+          { RocksDB.rocksMaxDiskSize = Just maxDiskSmall }
+    totalCapacity <- getTotalCapacity storageMocked
+    assertEqual
+      "RocksDB disk is limited"
+      (Just maxDiskSmall)
+      totalCapacity
+
+    -- limit is definitely bigger than actual disk size
+    -- actual disk capacity should be used
+    let maxDiskBig = maxBound :: Int -- ~8000 Petabytes
+        storageMocked = storage
+          { RocksDB.rocksMaxDiskSize = Just maxDiskBig }
+    fullDiskCapacity <- getDiskSize tmpDir
+    totalCapacity <- getTotalCapacity storageMocked
+    assertEqual
+      "RocksDB ignores too big disk limit"
+      (Just fullDiskCapacity)
+      totalCapacity
+
+    -- If totalCapacity is Nothing, Meta's internal load balacing will be
+    -- at risk since it depends on this value
+    assertBool "totalCapacity is not empty" (isJust totalCapacity)
 
 main :: IO ()
 main = withUnitTest $ testRunner $ TestList
-  [ TestLabel "Test no disk limit" $
-      TestCase $ withSystemTempDirectory "rocksdb-test" $ \tmpDir -> do
-        storage <- RocksDB.newStorage tmpDir def
-        assertEqual "No disk limit is set" Nothing
-          (RocksDB.rocksMaxDiskSize storage)
-
-        fullDiskCapacity <- getDiskSize tmpDir
-        Just rocksTotalCapacity <- getTotalCapacity storage
-        assertEqual "rocks has full disk capacity" fullDiskCapacity
-          rocksTotalCapacity
-
-  , TestLabel "Test disk limit with disk-to-mem ratio" $
-      TestCase $ withSystemTempDirectory "rocksdb-test" $ \tmpDir -> do
-        let config = def
-              { ServerConfig.config_db_rocksdb_disk_mem_capacity_ratio_limit =
-                  Just 20
-              }
-        storage <- RocksDB.newStorage tmpDir config
-        assertBool "Disk limit exist" $
-          isJust (RocksDB.rocksMaxDiskSize storage)
-
-  , TestLabel "Test getTotalCapacity" $ TestCase $
-      withSystemTempDirectory "rocksdb-test" $ \tmpDir -> do
-        storage <- RocksDB.newStorage tmpDir def
-
-        -- limit is definitely smaller than the actual disk size
-        -- limit will be applied
-        let maxDiskSmall = 2 * 1024 * 1024 * 1024 -- 2 GB
-            storageMocked = storage
-              { RocksDB.rocksMaxDiskSize = Just maxDiskSmall }
-        totalCapacity <- getTotalCapacity storageMocked
-        assertEqual
-          "RocksDB disk is limited"
-          (Just maxDiskSmall)
-          totalCapacity
-
-        -- limit is definitely bigger than actual disk size
-        -- actual disk capacity should be used
-        let maxDiskBig = maxBound :: Int -- ~8000 Petabytes
-            storageMocked = storage
-              { RocksDB.rocksMaxDiskSize = Just maxDiskBig }
-        fullDiskCapacity <- getDiskSize tmpDir
-        totalCapacity <- getTotalCapacity storageMocked
-        assertEqual
-          "RocksDB ignores too big disk limit"
-          (Just fullDiskCapacity)
-          totalCapacity
-
-        -- If totalCapacity is Nothing, Meta's internal load balacing will be
-        -- at risk since it depends on this value
-        assertBool "totalCapacity is not empty" (isJust totalCapacity)
+  [ TestLabel "Test no disk limit" testNoDiskLimit
+  , TestLabel "Test disk limit with ratio" testDiskLimitWithRatio
+  , TestLabel "Test getTotalCapacity" testGetTotalCapacity
   ]
