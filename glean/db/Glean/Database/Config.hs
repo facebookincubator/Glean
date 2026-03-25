2e0f7775710985b8230428f3133ed6f99215dca3
Simon Marlow <marlowsd@gmail.com>
2026-03-25 12:19:58 +0000

    Add internal query linter
    When working on the compiler it's easy to accidentally break something
    that results in a segfault. Most of these errors can be detected by
    checks on the Codegen IR, in particular correct type-safety and
    variable-scoping.
    
    - Add Glean.Query.Lint to implement query consistency checks
    - Turn on query linting with `GLEAN_DEBUG=lint` or `--debug-query-lint`
    - Linting is automatically enabled for all tests via `withTestEnv`

--- glean/db/Glean/Database/Config.hs
+++ glean/db/Glean/Database/Config.hs
@@ -204,15 +204,17 @@ data Config = Config
 data DebugFlags = DebugFlags
   { tcDebug :: !Bool
   , queryDebug :: !Bool
+  , queryLint :: !Bool
   }
 
 instance Default DebugFlags where
-  def = DebugFlags { tcDebug = False, queryDebug = False }
+  def = DebugFlags { tcDebug = False, queryDebug = False, queryLint = False }
 
 instance Semigroup DebugFlags where
   a <> b = DebugFlags
     { tcDebug = tcDebug a || tcDebug b
     , queryDebug = queryDebug a || queryDebug b
+    , queryLint = queryLint a || queryLint b
     }
 
 instance Monoid DebugFlags where
@@ -561,6 +563,7 @@ options = do
     debugParser = do
       tcDebug <- switch (long "debug-tc")
       queryDebug <- switch (long "debug-query")
+      queryLint <- switch (long "debug-query-lint")
       return DebugFlags{..}
 
     serverConfigThriftSource = option (eitherReader ThriftSource.parse)
