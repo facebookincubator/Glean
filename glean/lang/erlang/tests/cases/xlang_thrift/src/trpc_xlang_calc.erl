1e6447541b61243ea5c8527e41261e0cd152464c
Katie Ots <kjo@meta.com>
2026-07-30 04:06:38 -0700

    Bridge one thrift marker to all matching-kind decls in range
    Summary: Index all matching-kind decls for each thrift marker.
    
    Reviewed By: alanz
    
    Differential Revision: D114205110
    
    fbshipit-source-id: bbfeea2c6cc05c0d2f891211e5cab4cdd713f9d8

--- glean/lang/erlang/tests/cases/xlang_thrift/src/trpc_xlang_calc.erl
+++ glean/lang/erlang/tests/cases/xlang_thrift/src/trpc_xlang_calc.erl
@@ -3,8 +3,18 @@
 -module(trpc_xlang_calc).
 -codegen_source("fbcode/glean/lang/erlang/tests/cases/xlang_thrift/example.thrift").
 
--export([add/1]).
+-export([add/1, sub/2, sub/3, async_sub/2]).
 
 %% Glean {"file": "fbcode/glean/lang/erlang/tests/cases/xlang_thrift/example.thrift", "kind": "function", "service": "Calculator", "name": "add"}
 -spec add(map()) -> {ok, integer()}.
 add(_Req) -> {ok, 0}.
+
+%% Glean {"file": "fbcode/glean/lang/erlang/tests/cases/xlang_thrift/example.thrift", "kind": "function", "service": "Calculator", "name": "sub"}
+-spec sub(client, map()) -> {ok, integer()}.
+sub(_Client, _Req) -> {ok, 0}.
+
+-spec sub(client, map(), options) -> {ok, integer()}.
+sub(_Client, _Req, _Opts) -> {ok, 0}.
+
+-spec async_sub(client, map()) -> {ok, reference()}.
+async_sub(_Client, _Req) -> {ok, make_ref()}.
