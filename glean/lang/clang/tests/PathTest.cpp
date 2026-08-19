7da34f15c66d2030b52fc956781ba44c76b24249
generatedunixname1261038932274330 <noreply+1261038932274330@fb.com>
2026-08-19 03:38:14 -0700

    www/fbcode/glean/lang/clang/path.cpp
    Reviewed By: CatherineGasnier
    
    Differential Revision: D115391495
    
    fbshipit-source-id: 4e2033fc8b0aa1f8edd2df56411e681e00effac1

--- glean/lang/clang/tests/PathTest.cpp
+++ glean/lang/clang/tests/PathTest.cpp
@@ -11,82 +11,129 @@
 #include <gtest/gtest.h>
 #include <folly/testing/TestUtil.h>
 #include <filesystem>
+#include <string>
 
 namespace facebook::glean::clangx {
 
-TEST(PathTest, goodPath) {
-  static std::filesystem::path root =
+namespace {
+
+std::filesystem::path testRoot() {
 #ifdef _WIN32
-    "C:\\";
+  return "C:\\";
 #else
-    "/";
+  return "/";
 #endif
-
-  EXPECT_EQ(goodPath(root/"foo", "bar").generic_string(), "bar");
-
-  EXPECT_EQ(goodPath(root/"foo", "bar/../baz").generic_string(), "baz");
-
-  EXPECT_EQ(goodPath(root/"foo", "../foo/bar").generic_string(), "bar");
-  EXPECT_EQ(goodPath(root/"1/2/3", "../../2/3/foo").generic_string(), "foo");
-
-  EXPECT_EQ(goodPath(root/"1/2", root/"1/2/3/foo").generic_string(), "3/foo");
-  // FIXME: should this be ../bar?
-  EXPECT_EQ(goodPath(root/"foo", root/"bar").generic_string(), root/"bar");
-  // FIXME: should this be ../4/5?
-  EXPECT_EQ(goodPath(root/"1/2/3", root/"1/2/4/5").generic_string(), root/"1/2/4/5");
-
-  EXPECT_EQ(goodPath(root/"1/2/3","../4/5").generic_string(), "../4/5");
-
-  EXPECT_EQ(goodPath(root/"1/2/3", "4/../../3/5").generic_string(), "5");
-
-  // FIXME: don't know what we want here
-  EXPECT_TRUE(goodPath(root/"foo", root/"foo").empty());
-
-  EXPECT_TRUE(goodPath(root/"foo", "").empty());
 }
 
-TEST(PathTest, followSymlinksInsideRoot) {
+std::string normalized(const std::filesystem::path& path) {
+  return path.generic_string();
+}
+
+} // namespace
+
+TEST(PathTest, GoodPathLeavesRelativePathInsideRoot) {
+  const std::filesystem::path root = testRoot();
+
+  EXPECT_EQ(normalized(goodPath(root / "foo", "bar/../baz")), "baz");
+}
+
+TEST(PathTest, GoodPathRemovesRootSuffixFromRelativePath) {
+  const std::filesystem::path root = testRoot();
+
+  EXPECT_EQ(normalized(goodPath(root / "foo", "../foo/bar")), "bar");
+  EXPECT_EQ(normalized(goodPath(root / "1/2/3", "../../2/3/foo")), "foo");
+}
+
+TEST(PathTest, GoodPathKeepsRelativePathWhenItDoesNotResolveUnderRoot) {
+  const std::filesystem::path root = testRoot();
+
+  EXPECT_EQ(normalized(goodPath(root / "1/2/3", "../4/5")), "../4/5");
+}
+
+TEST(PathTest, GoodPathRelativizesAbsolutePathUnderRoot) {
+  const std::filesystem::path root = testRoot();
+
+  EXPECT_EQ(normalized(goodPath(root / "1/2", root / "1/2/3/foo")), "3/foo");
+  EXPECT_TRUE(goodPath(root / "foo", root / "foo").empty());
+}
+
+TEST(PathTest, GoodPathKeepsAbsolutePathOutsideRoot) {
+  const std::filesystem::path root = testRoot();
+
+  EXPECT_EQ(goodPath(root / "foo", root / "bar"), root / "bar");
+  EXPECT_EQ(goodPath(root / "1/2/3", root / "1/2/4/5"), root / "1/2/4/5");
+}
+
+TEST(PathTest, GoodPathKeepsEmptyPathEmpty) {
+  const std::filesystem::path root = testRoot();
+
+  EXPECT_TRUE(goodPath(root / "foo", "").empty());
+}
+
+TEST(PathTest, FollowSymlinksInsideRootLeavesPlainPathUnchanged) {
   using namespace std::filesystem;
 
   folly::test::TemporaryDirectory tmpdir;
-  auto dir = path(tmpdir.path().native());
+  const auto dir = path(tmpdir.path().native());
+  const auto root = dir / "root";
+  create_directories(root / "a/c");
 
-  auto root = dir/"root";
-  create_directory(root);
-
-  auto out = dir/"out";
-  create_directory(out);
-
-  create_directory(root/"a");
-  create_directory(root/"a/c");
-  create_directory_symlink("a", root/"b");
-  create_directory_symlink(out, root/"escape");
-
-  // symlink that traverses another symlink
-  create_directory_symlink("../b/c", root/"a/d");
-
-  // symlink going back into root
-  create_directory_symlink("../root/a/c", out/"back");
-
-  // no symlinks
   EXPECT_EQ(followSymlinksInsideRoot(root, path("a/c")), path("a/c"));
+}
 
-  // follow internal symlink
-  EXPECT_EQ(followSymlinksInsideRoot(root, path("b")), path("a"));
+TEST(PathTest, FollowSymlinksInsideRootResolvesInternalSymlinkWithTail) {
+  using namespace std::filesystem;
 
-  // follow dir symlink, relative and absolute
+  folly::test::TemporaryDirectory tmpdir;
+  const auto dir = path(tmpdir.path().native());
+  const auto root = dir / "root";
+  create_directories(root / "a/c");
+  create_directory_symlink("a", root / "b");
+
   EXPECT_EQ(followSymlinksInsideRoot(root, path("b/c")), path("a/c"));
+}
 
-  // don't follow external symlink
-  EXPECT_EQ(followSymlinksInsideRoot(root, path("escape")), path("escape"));
+TEST(PathTest, FollowSymlinksInsideRootResolvesNestedSymlink) {
+  using namespace std::filesystem;
 
-  // don't follow external dir symlink
-  EXPECT_EQ(followSymlinksInsideRoot(root, path("escape/c")), path("escape/c"));
+  folly::test::TemporaryDirectory tmpdir;
+  const auto dir = path(tmpdir.path().native());
+  const auto root = dir / "root";
+  create_directories(root / "a/c");
+  create_directory_symlink("a", root / "b");
+  create_directory_symlink("../b/c", root / "a/d");
 
-  // complicated case
   EXPECT_EQ(followSymlinksInsideRoot(root, path("a/d")), path("a/c"));
+}
 
-  // escape from root and then go back via a relative symlink
+TEST(PathTest, FollowSymlinksInsideRootLeavesEscapingSymlinkUnresolved) {
+  using namespace std::filesystem;
+
+  folly::test::TemporaryDirectory tmpdir;
+  const auto dir = path(tmpdir.path().native());
+  const auto root = dir / "root";
+  create_directory(root);
+  const auto out = dir / "out";
+  create_directory(out);
+  create_directory_symlink(out, root / "escape");
+
+  EXPECT_EQ(followSymlinksInsideRoot(root, path("escape")), path("escape"));
+  EXPECT_EQ(followSymlinksInsideRoot(root, path("escape/c")), path("escape/c"));
+}
+
+TEST(PathTest, FollowSymlinksInsideRootCanonicalizesEscapingPathBackIntoRoot) {
+  using namespace std::filesystem;
+
+  folly::test::TemporaryDirectory tmpdir;
+  const auto dir = path(tmpdir.path().native());
+  const auto root = dir / "root";
+  create_directories(root / "a/c");
+  const auto out = dir / "out";
+  create_directory(out);
+  create_directory_symlink(out, root / "escape");
+  create_directory_symlink("../root/a/c", out / "back");
+
   EXPECT_EQ(followSymlinksInsideRoot(root, path("escape/back")), path("a/c"));
 }
-}
+
+} // namespace facebook::glean::clangx
