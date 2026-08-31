e4d1777c2c77815ed96231e047ba671a7385bef0
Simon Marlow <marlowsd@gmail.com>
2026-08-31 19:06:15 +0100

    [buck2] timeout test

--- mk/gen-haskell-prebuilt.py
+++ mk/gen-haskell-prebuilt.py
@@ -249,7 +249,10 @@ def get_root_dep_ids():
             comp = c.get('component-name', '')
             wanted = (
                 (pkg in ('fb-util', 'fb-stubs') and comp.startswith('test:')) or
-                (pkg == 'glean' and (comp in ('exe:gen-schema', 'exe:glean') or comp.startswith('test:')))
+                (pkg == 'glean' and (
+                    comp in ('exe:gen-schema', 'exe:glean', 'lib:bench-lib', 'lib:bench-util') or
+                    comp.startswith('test:')
+                ))
             )
             if wanted:
                 for uid in c.get('depends', []):
