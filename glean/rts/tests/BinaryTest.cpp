29d6b9e99c571474755596f087d41e47f8ad1a3f
generatedunixname1261038932274330 <noreply+1261038932274330@fb.com>
2026-09-08 11:58:05 -0700

    www/fbcode/glean/rts/set.cpp
    Reviewed By: bochko
    
    Differential Revision: D119055880
    
    fbshipit-source-id: 899199a712f6197bfb147a60849efa57743fd6ac

--- glean/rts/tests/BinaryTest.cpp
+++ glean/rts/tests/BinaryTest.cpp
@@ -7,10 +7,13 @@
  */
 
 #include "glean/rts/binary.h"
+#include "glean/rts/set.h"
 
 #include <glog/logging.h>
 #include <gtest/gtest.h>
+#include <cstdint>
 #include <string>
+#include <vector>
 
 namespace facebook {
 namespace glean {
@@ -27,6 +30,36 @@ void roundtripString(const std::string& s) {
   CHECK_EQ(s, binary::mkString(demangled.bytes()));
 }
 
+binary::Output packedValue(uint64_t value) {
+  binary::Output output;
+  output.packed(value);
+  return output;
+}
+
+std::vector<uint64_t> decodePackedArray(const binary::Output& output) {
+  binary::Input input(output.bytes());
+  const auto size = input.packed<uint64_t>();
+  std::vector<uint64_t> values;
+  values.reserve(size);
+  for (uint64_t i = 0; i < size; ++i) {
+    values.push_back(input.packed<uint64_t>());
+  }
+  EXPECT_TRUE(input.empty());
+  return values;
+}
+
+std::vector<uint8_t> decodeByteArray(const binary::Output& output) {
+  binary::Input input(output.bytes());
+  const auto size = input.packed<uint64_t>();
+  std::vector<uint8_t> values;
+  values.reserve(size);
+  for (uint64_t i = 0; i < size; ++i) {
+    values.push_back(input.fixed<uint8_t>());
+  }
+  EXPECT_TRUE(input.empty());
+  return values;
+}
+
 } // namespace
 
 using namespace std::string_literals;
@@ -37,6 +70,131 @@ TEST(BinaryTest, utf8String) {
   roundtripString("\0"s);
   roundtripString("abc\0def"s);
   roundtripString("abc\0\0");
+}
+
+TEST(SetOpsTest, OutputSetDeduplicatesAndUsesSerializedByteOrder) {
+  SetOps sets;
+  const auto token = sets.newSet();
+  auto highByteFirst = packedValue(255);
+  auto lowByteFirst = packedValue(256);
+  auto duplicate = packedValue(255);
+
+  sets.insertOutputSet(token, &highByteFirst);
+  sets.insertOutputSet(token, &lowByteFirst);
+  sets.insertOutputSet(token, &duplicate);
+  binary::Output encoded;
+  sets.setToArray(token, &encoded);
+
+  const std::vector<uint64_t> expected{256, 255};
+  EXPECT_EQ(decodePackedArray(encoded), expected);
+}
+
+TEST(SetOpsTest, OutputSetRejectsInsertionPastByteLimitWithoutMutation) {
+  SetOps sets(2);
+  const auto token = sets.newSet();
+  auto accepted = packedValue(128);
+  sets.insertOutputSet(token, &accepted);
+  auto rejected = packedValue(1);
+
+  EXPECT_THROW(sets.insertOutputSet(token, &rejected), std::runtime_error);
+  binary::Output encoded;
+  sets.setToArray(token, &encoded);
+
+  const std::vector<uint64_t> expected{128};
+  EXPECT_EQ(decodePackedArray(encoded), expected);
+}
+
+TEST(SetOpsTest, FreeSetCompactsTokensAndTheirSizeAccountingTogether) {
+  SetOps sets(1);
+  const auto discardedToken = sets.newSet();
+  const auto retainedToken = sets.newSet();
+  auto discarded = packedValue(1);
+  auto retained = packedValue(2);
+  sets.insertOutputSet(discardedToken, &discarded);
+  sets.insertOutputSet(retainedToken, &retained);
+
+  sets.freeSet(discardedToken);
+  auto overLimit = packedValue(3);
+  EXPECT_THROW(sets.insertOutputSet(0, &overLimit), std::runtime_error);
+  binary::Output encoded;
+  sets.setToArray(0, &encoded);
+
+  const std::vector<uint64_t> expected{2};
+  EXPECT_EQ(decodePackedArray(encoded), expected);
+}
+
+TEST(SetOpsTest, WordSetDeduplicatesAndSerializesInNumericOrder) {
+  SetOps sets;
+  const auto token = sets.newWordSet();
+
+  sets.insertWordSet(token, 42);
+  sets.insertWordSet(token, 42);
+  sets.insertWordSet(token, 7);
+  binary::Output encoded;
+  sets.wordSetToArray(token, &encoded);
+
+  const std::vector<uint64_t> expected{7, 42};
+  EXPECT_EQ(decodePackedArray(encoded), expected);
+}
+
+TEST(SetOpsTest, WordSetRejectsValuePastSizeLimitWithoutMutation) {
+  SetOps sets(2 * sizeof(uint64_t));
+  const auto token = sets.newWordSet();
+  sets.insertWordSet(token, 5);
+  sets.insertWordSet(token, 10);
+
+  EXPECT_THROW(sets.insertWordSet(token, 15), std::runtime_error);
+  binary::Output encoded;
+  sets.wordSetToArray(token, &encoded);
+
+  const std::vector<uint64_t> expected{5, 10};
+  EXPECT_EQ(decodePackedArray(encoded), expected);
+}
+
+TEST(SetOpsTest, ByteSetDeduplicatesAndSerializesAsSortedBytes) {
+  SetOps sets;
+  const auto token = sets.newWordSet();
+  const unsigned char values[]{255, 3, 3, 0};
+
+  sets.insertBytesWordSet(token, values, values + std::size(values));
+  binary::Output encoded;
+  sets.byteSetToByteArray(token, &encoded);
+
+  const std::vector<uint8_t> expected{0, 3, 255};
+  EXPECT_EQ(decodeByteArray(encoded), expected);
+}
+
+TEST(SetOpsTest, ByteSetRejectsOversizedBatchWithoutPartialInsertion) {
+  SetOps sets(2);
+  const auto token = sets.newWordSet();
+  const unsigned char oversized[]{1, 2, 3};
+
+  EXPECT_THROW(
+      sets.insertBytesWordSet(
+          token, oversized, oversized + std::size(oversized)),
+      std::runtime_error);
+  const unsigned char accepted[]{4, 5};
+  sets.insertBytesWordSet(token, accepted, accepted + std::size(accepted));
+  binary::Output encoded;
+  sets.byteSetToByteArray(token, &encoded);
+
+  const std::vector<uint8_t> expected{4, 5};
+  EXPECT_EQ(decodeByteArray(encoded), expected);
+}
+
+TEST(SetOpsTest, FreeWordSetMakesTheFollowingSetTheFirstToken) {
+  SetOps sets;
+  const auto discardedToken = sets.newWordSet();
+  const auto retainedToken = sets.newWordSet();
+  sets.insertWordSet(discardedToken, 1);
+  sets.insertWordSet(retainedToken, 2);
+
+  sets.freeWordSet(discardedToken);
+  binary::Output encoded;
+  sets.wordSetToArray(0, &encoded);
+
+  const std::vector<uint64_t> expected{2};
+  EXPECT_EQ(decodePackedArray(encoded), expected);
 }
 
 } // namespace rts
